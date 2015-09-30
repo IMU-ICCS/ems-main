11fc87b754188e55595456ff9464a0deee1313ad
Etienne Charlier <etienne.charlier@cetic.be>
2015-09-30 15:45:02 +0200

    Initial commit

---
+++ src/main/java/eu/paasage/upperware/metasolver/messageListener.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2014-5 UK Science and Technology Facilities Council
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package eu.paasage.upperware.metasolver;
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+import org.jeromq.*;
+public class messageListener {
+
+	Context cntx = null;
+	ZMQ zmq = new ZMQ();
+
+	public String subs(){
+
+		Context cntx = zmq.context(1);
+
+		Socket	subscriber = cntx.socket (zmq.SUB);
+		subscriber.connect ("tcp://metricscollectior?:5563");
+		subscriber.subscribe(null);//need topic here 
+		while (!Thread.currentThread ().isInterrupted ()) {
+            // Read envelope with address
+            String address = subscriber.recvStr ();
+            // Read message contents
+            String contents = subscriber.recvStr ();
+            System.out.println(address + " : " + contents);
+        }
+        subscriber.close ();
+        cntx.term ();
+    
+
+		return null;
+	}
+
+}
---
+++ src/main/java/eu/paasage/upperware/metasolver/metasolver.java
@@ -0,0 +1,82 @@
+
+/*
+ * Copyright (c) 2014-5 UK Science and Technology Facilities Council
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package eu.paasage.upperware.metasolver;
+
+//package org.ow2.paasage.upperware.metasolver;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+import org.eclipse.emf.cdo.view.CDOView;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+
+//The metasolver is responsible for calling the different solvers in PaaSage.
+
+public class metasolver{
+
+
+//Currently we only have one solver and this is invoked taking in the ResourceID from the masterscript	
+public static void main(String args[]) throws IOException, InterruptedException{
+	
+	try{
+	
+			Process p1 = Runtime.getRuntime().exec("java -jar milp-solver-2015.04-SNAPSHOT-assembly.jar " + args[1]);
+			 
+		     // you can pass the system command or a script to exec command. here i used uname -a system command
+		             BufferedReader stdInput = new BufferedReader(new
+		             InputStreamReader(p1.getInputStream()));
+
+		     BufferedReader stdError = new BufferedReader(new
+		             InputStreamReader(p1.getErrorStream()));
+
+		     // read the output from the command
+		     String s1="";
+		     StringBuilder sb = new StringBuilder(); 
+		     while ((s1 = stdInput.readLine()) != null) {
+		        
+		         sb.append(s1);
+		            sb.append("\n");
+		     }
+
+		     while ((s1 = stdError.readLine()) != null) {
+		         
+		         sb.append(s1);
+		            sb.append("\n");
+		     }
+
+		     
+		     s1= sb.toString();
+		System.out.println(" output = " + s1);
+		     
+		     if (s1.length() > 1 || Integer.parseInt(s1) > 0){
+		    	 
+		    //		Process p2 = Runtime.getRuntime().exec("./LAStart " + args[1]);
+                    String s2 = "";
+    	     while ((s2 = stdInput.readLine()) != null) {
+    	    	 
+    	         System.out.println("Std OUT: "+s2);
+    	     }
+
+    	     while ((s2 = stdError.readLine()) != null) {
+    	         System.out.println("Std ERROR : "+s2);
+    	     }
+
+     }
+
+ } catch (IOException e) {
+
+     e.printStackTrace();
+ }
+	
+}
+}
