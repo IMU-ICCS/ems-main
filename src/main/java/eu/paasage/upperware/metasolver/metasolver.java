645ad55c5f8bcd0eaa6cb514dec15536cdcb34b6
unknown <tk78@sclt029.escdc.fed.cclrc.ac.uk>
2015-10-19 16:15:53 +0100

    added support for zeroMQ

--- src/main/java/eu/paasage/upperware/metasolver/metasolver.java
+++ src/main/java/eu/paasage/upperware/metasolver/metasolver.java
@@ -16,6 +16,7 @@ import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
+
 import org.eclipse.emf.cdo.view.CDOView;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
@@ -23,89 +24,82 @@ import org.eclipse.emf.ecore.EObject;
 //The metasolver is responsible for calling the different solvers in PaaSage.
 
 public class metasolver{
+	static metricsListener ml = new metricsListener("metricID");
+	static solutionsListener sl = new solutionsListener("metricID");
+	static adaptorListener al = new adaptorListener("metricID");
+	static RPListener rpl = new RPListener("metricID");
+	//Currently we only have one solver and this is invoked taking in the ResourceID from the masterscript	
 
 
-//Currently we only have one solver and this is invoked taking in the ResourceID from the masterscript	
-public static void main(String args[]) throws IOException, InterruptedException{
-	
-	
-	
-	//first we subscribe
-	
-	//Rule Processor
-	if (args[0].contains("withZEROMQ")){
-		RPListener rpl = new RPListener("metricID");
-		rpl.run();
+	public static void main(String args[]) throws IOException, InterruptedException{
+
+
+		if (args[0].contains("with ZeroMQ")){
+			go();
+
+
+			System.out.println("all subscriptions complete");
 		}
-				
-	//Solvers
-	if (args[0].contains("withZEROMQ")){
-	solutionListener sl = new solutionListener("metricID");
-	sl.run();
+		else{
+			try{
+
+				//		Process p1 = Runtime.getRuntime().exec("java -jar milp-solver-2015.04-SNAPSHOT-assembly.jar " + args[1]);
+				Process p1 = Runtime.getRuntime().exec("java -jar milp-solver-assembly.jar " + args[1]);
+
+				// you can pass the system command or a script to exec command. here i used uname -a system command
+				BufferedReader stdInput = new BufferedReader(new
+						InputStreamReader(p1.getInputStream()));
+
+				BufferedReader stdError = new BufferedReader(new
+						InputStreamReader(p1.getErrorStream()));
+
+				// read the output from the command
+				String s1="";
+				StringBuilder sb = new StringBuilder(); 
+				while ((s1 = stdInput.readLine()) != null) {
+
+					sb.append(s1);
+					sb.append("\n");
+				}
+
+				while ((s1 = stdError.readLine()) != null) {
+
+					sb.append(s1);
+					sb.append("\n");
+				}
+
+
+				s1= sb.toString();
+				System.out.println(" output = " + s1);
+
+				if (s1.length() > 1 || Integer.parseInt(s1) > 0){
+
+					//		Process p2 = Runtime.getRuntime().exec("./LAStart " + args[1]);
+					String s2 = "";
+					while ((s2 = stdInput.readLine()) != null) {
+
+						System.out.println("Std OUT: "+s2);
+					}
+
+					while ((s2 = stdError.readLine()) != null) {
+						System.out.println("Std ERROR : "+s2);
+					}
+
+				}
+
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
 	}
-	
-	//Adaptor
-	if (args[0].contains("withZEROMQ")){
-		adaptorListener sl = new adaptorListener("metricID");
-		sl.run();
-		}
-	
-	//MetricsCollector
-	if (args[0].contains("withZEROMQ")){
-		metricsListener sl = new metricsListener("metricID");
-		sl.run();
-		}
-	
-	try{
-	
-	//		Process p1 = Runtime.getRuntime().exec("java -jar milp-solver-2015.04-SNAPSHOT-assembly.jar " + args[1]);
-			Process p1 = Runtime.getRuntime().exec("java -jar milp-solver-assembly.jar " + args[1]);
-			 
-		     // you can pass the system command or a script to exec command. here i used uname -a system command
-		             BufferedReader stdInput = new BufferedReader(new
-		             InputStreamReader(p1.getInputStream()));
+	public static void go(){
 
-		     BufferedReader stdError = new BufferedReader(new
-		             InputStreamReader(p1.getErrorStream()));
+		rpl.start();
+		ml.start();
+		al.start();
+		sl.start();
 
-		     // read the output from the command
-		     String s1="";
-		     StringBuilder sb = new StringBuilder(); 
-		     while ((s1 = stdInput.readLine()) != null) {
-		        
-		         sb.append(s1);
-		            sb.append("\n");
-		     }
+	}
 
-		     while ((s1 = stdError.readLine()) != null) {
-		         
-		         sb.append(s1);
-		            sb.append("\n");
-		     }
-
-		     
-		     s1= sb.toString();
-		System.out.println(" output = " + s1);
-		     
-		     if (s1.length() > 1 || Integer.parseInt(s1) > 0){
-		    	 
-		    //		Process p2 = Runtime.getRuntime().exec("./LAStart " + args[1]);
-                    String s2 = "";
-    	     while ((s2 = stdInput.readLine()) != null) {
-    	    	 
-    	         System.out.println("Std OUT: "+s2);
-    	     }
-
-    	     while ((s2 = stdError.readLine()) != null) {
-    	         System.out.println("Std ERROR : "+s2);
-    	     }
-
-     }
-
- } catch (IOException e) {
-
-     e.printStackTrace();
- }
-	
-}
 }
