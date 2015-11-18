885fd3f632fb3c90d1d05c340e45437844fda0fb
Dennis Hoppe <dennis.hoppe@hlrs.de>
2015-11-18 15:49:10 +0100

    added initial draft for validation of simple expression

---
+++ src/main/java/eu/paasage/upperware/profiler/rp/algebra/UnsolvableException.java
@@ -0,0 +1,10 @@
+package eu.paasage.upperware.profiler.rp.algebra;
+
+public class UnsolvableException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2155681274303835137L;
+
+}
