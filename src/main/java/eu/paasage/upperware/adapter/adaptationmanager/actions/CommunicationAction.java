70346fbb35eef967a1ab8bd49aea8e0125040a82
Nikos Parlavantzas <Nikos.Parlavantzas@irisa.fr>
2015-11-27 16:28:49 +0100

    small correction

--- src/main/java/eu/paasage/upperware/adapter/adaptationmanager/actions/CommunicationAction.java
+++ src/main/java/eu/paasage/upperware/adapter/adaptationmanager/actions/CommunicationAction.java
@@ -112,7 +112,7 @@ public class CommunicationAction implements Action {
 				
 				LOGGER.log(Level.INFO, "Created provider port : ID " + provPortID);
 				
-				if(provPortID != null && provPortID != "" && dataShare.setProvPortID(provPortName, provPortID))
+				if(provPortID != null && !provPortID.equals("")  && dataShare.setProvPortID(provPortName, provPortID))
 					LOGGER.log(Level.INFO, "Stored newly created provider port Instance : ID " + provPortID);
 				else
 					LOGGER.log(Level.WARNING, "Could not store newly created provider port Instance : ID " + provPortID);
@@ -130,7 +130,7 @@ public class CommunicationAction implements Action {
 				
 				LOGGER.log(Level.INFO, "Created required port : ID " + consPortID);
 				
-				if(consPortID != null && consPortID != "" && dataShare.setReqPortID(consPortName, consPortID))
+				if(consPortID != null && !consPortID.equals("") && dataShare.setReqPortID(consPortName, consPortID))
 					LOGGER.log(Level.INFO, "Stored newly created required port Instance : ID " + consPortID);
 				else
 					LOGGER.log(Level.WARNING, "Could not store newly created required port Instance : ID " + consPortID);
@@ -145,7 +145,7 @@ public class CommunicationAction implements Action {
 				
 				LOGGER.log(Level.INFO, "Created Communication : ID " + commExecID);
 				
-				if(commExecID != null && commExecID != "" && dataShare.setCommID(communicationName, execInterfacer.trimResponseID(commExecID)))
+				if(commExecID != null && !commExecID.equals("") && dataShare.setCommID(communicationName, execInterfacer.trimResponseID(commExecID)))
 					LOGGER.log(Level.INFO, "Stored newly created Communication Type Instance : ID " + commExecID);
 				else
 					LOGGER.log(Level.WARNING, "Could not store newly created Communication Type Instance : ID " + commExecID);
