80dd467244517a300490e7e987d8dbe6d9f14ab8
Nikos Parlavantzas <Nikos.Parlavantzas@irisa.fr>
2015-11-29 16:39:52 +0100

    plan validation

--- src/main/java/eu/paasage/upperware/adapter/adaptationmanager/validation/ValidatorImpl.java
+++ src/main/java/eu/paasage/upperware/adapter/adaptationmanager/validation/ValidatorImpl.java
@@ -14,14 +14,41 @@ import java.util.logging.Logger;
 //import eu.paasage.upperware.adapter.adaptationmanager.plangeneration.Plan;
 import eu.paasage.upperware.plangenerator.model.Plan;
 
-
 public class ValidatorImpl implements IValidator {
 
+	public long estimatedTimeToNext = 0; // ms
 	private final static Logger LOGGER = Logger.getLogger(ValidatorImpl.class
 			.getName());
+	private static final double alpha = 0.9;
+	private long meanTaskDuration; // ms
+	private long lastTime = 0; // ns
+	private int count = 0;
+	
+	public ValidatorImpl(int duration) {
+		this.meanTaskDuration = duration;
+	}
+
+	public ValidatorImpl() {
+		this.meanTaskDuration = 0;
+	}
 
 	public boolean validate(Plan p) {
-//		LOGGER.log(Level.INFO, "Validated plan");
-		return true;
+		boolean validation = false;
+		long now = System.nanoTime();
+		long timeFromPrevious = (now - lastTime) / 1000000;
+		lastTime = now;
+
+		if (++count <= 2) {
+			estimatedTimeToNext=timeFromPrevious;
+			validation=true;
+		}
+		else {
+			long predictedPlanDuration = p.getTasks().size() * meanTaskDuration;
+			estimatedTimeToNext = (long) (alpha * timeFromPrevious + (1 - alpha)
+					* estimatedTimeToNext);
+			validation = (predictedPlanDuration <= estimatedTimeToNext);
+		}
+		LOGGER.log(Level.INFO, "Plan validation:"+validation);
+		return validation;
 	}
 }
