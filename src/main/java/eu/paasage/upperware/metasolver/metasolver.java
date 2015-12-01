ab06cf89c0c6c78b4052ffb8aa917efeacb3f65b
unknown <tk78@sclt029.escdc.fed.cclrc.ac.uk>
2015-12-01 18:53:52 +0000

    added metric support to ms

--- src/main/java/eu/paasage/upperware/metasolver/metasolver.java
+++ src/main/java/eu/paasage/upperware/metasolver/metasolver.java
@@ -13,13 +13,24 @@ import org.zeromq.ZMQ;
 import org.zeromq.ZMQ.Context;
 import org.zeromq.ZMQ.Socket;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.List;
 
+import eu.paasage.upperware.metamodel.cp.ConstraintProblem;
+import eu.paasage.upperware.metamodel.cp.MetricVariable;
+import eu.paasage.upperware.metamodel.cp.MetricVariableValue;
+import eu.paasage.upperware.metamodel.cp.Solution;
+import eu.paasage.upperware.metasolver.exception.MetricMapperException;
+import eu.paasage.upperware.metasolver.util.CdoTool;
+import eu.paasage.upperware.metasolver.util.CpModelTool;
+import eu.paasage.upperware.profiler.cp.generator.model.tools.CPModelTool;
 import eu.paasage.upperware.metasolver.solutionListener;
 import eu.paasage.upperware.metasolver.RPListener;
 import eu.paasage.upperware.metasolver.adaptorListener;
@@ -49,7 +60,7 @@ public class metasolver{
 			 */
 			JsonObject jObj = map.mapMetricVariables(CPmodID);
 
-			if(args.length > 2){
+			if(args.length == 2){
 				if (Integer.parseInt(args[2]) <= 1){
 					runMILPSolver(jObj.get("id").asString(), jObj.get("solution_tmp").asLong());
 				}
@@ -58,6 +69,28 @@ public class metasolver{
 
 				}
 			}	 
+
+			if(args.length == 3){
+				int p = 0;
+				//get Metrics
+				HashMap mets = readFile1(args[3]);
+				//check Metrics
+				String check = checkMets(mets, args[1]);
+				String value = (String) mets.get(check);
+				if (value != null){
+					System.out.println("metric = " +value);
+
+					JsonObject jObj2 = map.mapMetricVariables(args[2], mets);
+					if (Integer.parseInt(args[2]) <= 1){
+						runMILPSolver(jObj.get("id").asString(), jObj2.get("solution_tmp").asLong());
+					}
+					if(Integer.parseInt(args[2]) >= 2){
+						runCPSolver(CPmodID, jObj2.get("solution_tmp").asLong());
+
+					}
+				}
+				else{ System.out.println("no match");}	
+			}
 
 
 			else{
@@ -297,4 +330,46 @@ public class metasolver{
 
 
 	}
+	public static HashMap readFile1(String fin) throws IOException {
+		FileInputStream fis = new FileInputStream(fin);
+		HashMap tt = new HashMap<String, String>();
+
+		//Construct BufferedReader from InputStreamReader
+		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+		int i=0;
+		String[] line = null;
+		String d = null;
+		while ((d = br.readLine()) != null) {	
+			String[] t = d.split(":");				
+			tt.put(t[0],t[1]);	
+			i++;
+		}
+
+		br.close();
+		return tt;
+	}
+	public static String checkMets(HashMap mets, String cpModel){
+		CdoTool utils = CdoTool.getInstance();
+
+
+		String metricId = null;
+		List<EObject> model_contents = null;		
+		// start the cdo-client
+		try{
+			utils.openCDOSession();
+			// clone the resource in memory and get the cp model 
+			model_contents = utils.cloneModel(cpModel); // cloner may return an empty list		
+			ConstraintProblem cp = CpModelTool.getCPModel(model_contents);
+
+			for(MetricVariable mv : cp.getMetricVariables()) {
+				metricId = mv.getId();
+
+			}
+		}catch (Exception e){
+			System.out.println("error" + e);	
+		}
+
+		return metricId;
+	}
+
 }
