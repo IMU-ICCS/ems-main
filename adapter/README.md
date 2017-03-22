b3e3680d7b0764cbe173b28c06345bfe496fa0aa
Maciej Miodowski <mmiodowski@7bulls.com>
2017-03-22 12:38:03 +0100

    Spring Boot Adapter Application template

---
+++ adapter/README.md
@@ -0,0 +1,3 @@
+Adapter
+========
+Component of PaaSage responsible for generating, validating and applying deployment plan based on CAMEL Model received from CDO Server. This component displaces both Adapter Manager and Plan Generator repositories.
---
+++ adapter/src/main/java/eu/paasage/upperware/adapter/AdapterApplication.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2017 7bulls
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL
+ * was not distributed with this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ */
+
+package eu.paasage.upperware.adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AdapterApplication.class, args);
+  }
+}
