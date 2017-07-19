477e17c1c67c48cd0579c1d5e62226e192bbfb21
Maciej Miodowski <mmiodowski@7bulls.com>
2017-07-19 12:16:22 +0200

    Health check

---
+++ adapter/src/main/java/eu/paasage/upperware/adapter/AdapterController.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2017 7bulls.com
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL
+ * was not distributed with this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ */
+
+package eu.paasage.upperware.adapter;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class AdapterController {
+
+  @RequestMapping(value = "/health", method = GET)
+  public void health() {
+
+  }
+}
