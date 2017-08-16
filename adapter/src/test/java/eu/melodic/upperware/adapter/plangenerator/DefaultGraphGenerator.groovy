44cc8cf3d5262f4f62188ced00c826a9e835adda
mmaczkowska <mmaczkowska@7bulls.com>
2017-08-16 17:15:33 +0200

    refactor and new test

--- adapter/src/test/java/eu/melodic/upperware/adapter/plangenerator/DefaultGraphGenerator.groovy
+++ adapter/src/test/java/eu/melodic/upperware/adapter/plangenerator/DefaultGraphGenerator.groovy
@@ -38,74 +38,66 @@ class DefaultGraphGeneratorTests extends Specification {
   POJOCreatorExample c
   Map<GraphValidator.TASK_TYPE, Set<Task>> tasks
   Map<GraphValidator.TASK_TYPE, Set<GraphValidator.TASK_TYPE>> dependencies
+  DefaultGraphGenerator generator
 
   def setup(){
     c = new POJOCreatorExample()
     tasks = GraphValidator.initMap()
     dependencies = GraphValidator.createDependencies()
+    generator = new DefaultGraphGenerator()
   }
 
   def "graph generation: all components"() {
 
     setup:
-    Collection<CloudApi> cloudApis =
-            Lists.newArrayList(c.createApi(apiName, tasks))
-    Collection<Cloud> clouds =
-            Lists.newArrayList(c.createCloud(cloudName, apiName, tasks))
-    Collection<CloudProperty> cloudProperties =
-            Lists.newArrayList(c.createCloudProperty(cloudPropName, cloudName, tasks))
-    Collection<CloudCredential> cloudCredentials =
-            Lists.newArrayList(c.createCloudCredential(cloudCredName, cloudName, tasks))
+    cloudApis = Lists.newArrayList(
+            c.createApi(apiName, tasks))
+    clouds = Lists.newArrayList(
+            c.createCloud(cloudName, apiName, tasks))
+    cloudProperties = Lists.newArrayList(
+            c.createCloudProperty(cloudPropName, cloudName, tasks))
+    cloudCredentials = Lists.newArrayList(
+            c.createCloudCredential(cloudCredName, cloudName, tasks))
 
     Application application =
             c.createApplication(appName, tasks)
     ApplicationInstance applicationInstance =
             c.createApplicationInstance(appInstName, appName, tasks)
 
-    Collection<LifecycleComponent> lifecycleComponents =
-            Lists.newArrayList(
-                    c.createLifecycleComponent(lifecycleName, tasks),
-                    c.createLifecycleComponent(lifecycleName2, tasks))
+    lifecycleComponents = Lists.newArrayList(
+            c.createLifecycleComponent(lifecycleName, tasks),
+            c.createLifecycleComponent(lifecycleName2, tasks))
 
-    Collection<VirtualMachine> virtualMachines =
-            (Lists.newArrayList(
-                    c.createVirtualMachine(vmName, cloudName, tasks),
-                    c.createVirtualMachine(vmName2, cloudName, tasks)))
-    Collection<VirtualMachineInstance> virtualMachineInstances =
-            Lists.newArrayList(
-                    c.createVirtualMachineInstance(vmInstName, vmName, tasks),
-                    c.createVirtualMachineInstance(vmInstName2, vmName2, tasks))
+    virtualMachines = Lists.newArrayList(
+            c.createVirtualMachine(vmName, cloudName, tasks),
+            c.createVirtualMachine(vmName2, cloudName, tasks))
+    virtualMachineInstances = Lists.newArrayList(
+            c.createVirtualMachineInstance(vmInstName, vmName, tasks),
+            c.createVirtualMachineInstance(vmInstName2, vmName2, tasks))
 
-    Collection<ApplicationComponent> applicationComponents =
-            Lists.newArrayList(
-                    c.createApplicationComponent(appCompName, appName, lifecycleName, vmName, tasks),
-                    c.createApplicationComponent(appCompName2, appName, lifecycleName2, vmName2, tasks))
+    applicationComponents = Lists.newArrayList(
+            c.createApplicationComponent(appCompName, appName, lifecycleName, vmName, tasks),
+            c.createApplicationComponent(appCompName2, appName, lifecycleName2, vmName2, tasks))
 
-    Collection<ApplicationComponentInstance> applicationComponentInstances =
-            Lists.newArrayList(
-                    c.createApplicationComponentInstance(
-                            appCompInstName, appName, vmInstName, appCompName, tasks),
-                    c.createApplicationComponentInstance(
-                            appCompInstName2, appName, vmInstName2, appCompName2, tasks))
+    applicationComponentInstances = Lists.newArrayList(
+            c.createApplicationComponentInstance(
+                    appCompInstName, appName, vmInstName, appCompName, tasks),
+            c.createApplicationComponentInstance(
+                    appCompInstName2, appName, vmInstName2, appCompName2, tasks))
 
-    Collection<Communication> communications =
-            Lists.newArrayList(c.createCommunication(
-                    communicationName, portProvidedName, portRequiredName, tasks))
-    Collection<PortProvided> portsProvided =
-            Lists.newArrayList(
-                    c.createPortProvided(portProvidedName, appCompName, tasks),
-                    c.createPortProvided(portProvidedName2, appCompName2, tasks))
-    Collection<PortRequired> portsRequired =
-            Lists.newArrayList(c.createPortRequired(portRequiredName, appCompName, tasks))
+    communications = Lists.newArrayList(c.createCommunication(
+            communicationName, portProvidedName, portRequiredName, tasks))
+    portsProvided = Lists.newArrayList(
+            c.createPortProvided(portProvidedName, appCompName, tasks),
+            c.createPortProvided(portProvidedName2, appCompName2, tasks))
+    portsRequired = Lists.newArrayList(c.createPortRequired(portRequiredName, appCompName, tasks))
 
-    Collection<VirtualMachineInstanceMonitor> virtualMachineInstanceMonitors =
-            Lists.newArrayList(
-                    c.toMonitor1(vmInstName, tasks),
-                    c.toMonitor1(vmInstName2, tasks))
-    Collection<ApplicationComponentInstanceMonitor> applicationComponentInstanceMonitors =
-            Lists.newArrayList(
-                    c.toMonitor3(appCompInstName, tasks),
-                    c.toMonitor3(appCompInstName2, tasks))
+    virtualMachineInstanceMonitors = Lists.newArrayList(
+            c.toMonitor1(vmInstName, tasks),
+            c.toMonitor1(vmInstName2, tasks))
+    applicationComponentInstanceMonitors = Lists.newArrayList(
+            c.toMonitor3(appCompInstName, tasks),
+            c.toMonitor3(appCompInstName2, tasks))
 
     ComparableModel model = createModel(
             cloudApis, clouds, cloudProperties, cloudCredentials,
@@ -114,7 +106,6 @@ class DefaultGraphGeneratorTests extends Specification {
             applicationComponentInstances, communications, portsProvided,
             portsRequired, virtualMachineInstanceMonitors,
             applicationComponentInstanceMonitors)
-    DefaultGraphGenerator generator = new DefaultGraphGenerator()
 
     when:
     SimpleDirectedGraph<Task, DefaultEdge> graph = generator.generateConfigGraph(model)
@@ -128,58 +119,47 @@ class DefaultGraphGeneratorTests extends Specification {
   def "graph generation: without communication"() {
 
     setup:
-    Collection<CloudApi> cloudApis =
-            Lists.newArrayList(c.createApi(apiName, tasks))
-    Collection<Cloud> clouds =
-            Lists.newArrayList(c.createCloud(cloudName, apiName, tasks))
-    Collection<CloudProperty> cloudProperties =
-            Lists.newArrayList(c.createCloudProperty(cloudPropName, cloudName, tasks))
-    Collection<CloudCredential> cloudCredentials =
-            Lists.newArrayList(c.createCloudCredential(cloudCredName, cloudName, tasks))
+    cloudApis = Lists.newArrayList(
+            c.createApi(apiName, tasks))
+    clouds = Lists.newArrayList(
+            c.createCloud(cloudName, apiName, tasks))
+    cloudProperties = Lists.newArrayList(
+            c.createCloudProperty(cloudPropName, cloudName, tasks))
+    cloudCredentials = Lists.newArrayList(
+            c.createCloudCredential(cloudCredName, cloudName, tasks))
 
     Application application =
             c.createApplication(appName, tasks)
     ApplicationInstance applicationInstance =
             c.createApplicationInstance(appInstName, appName, tasks)
 
-    Collection<LifecycleComponent> lifecycleComponents =
-            Lists.newArrayList(
-                    c.createLifecycleComponent(lifecycleName, tasks),
-                    c.createLifecycleComponent(lifecycleName2, tasks))
+    lifecycleComponents = Lists.newArrayList(
+            c.createLifecycleComponent(lifecycleName, tasks),
+            c.createLifecycleComponent(lifecycleName2, tasks))
 
-    Collection<VirtualMachine> virtualMachines =
-            (Lists.newArrayList(
-                    c.createVirtualMachine(vmName, cloudName, tasks),
-                    c.createVirtualMachine(vmName2, cloudName, tasks)))
-    Collection<VirtualMachineInstance> virtualMachineInstances =
-            Lists.newArrayList(
-                    c.createVirtualMachineInstance(vmInstName, vmName, tasks),
-                    c.createVirtualMachineInstance(vmInstName2, vmName2, tasks))
+    virtualMachines = Lists.newArrayList(
+            c.createVirtualMachine(vmName, cloudName, tasks),
+            c.createVirtualMachine(vmName2, cloudName, tasks))
+    virtualMachineInstances = Lists.newArrayList(
+            c.createVirtualMachineInstance(vmInstName, vmName, tasks),
+            c.createVirtualMachineInstance(vmInstName2, vmName2, tasks))
 
-    Collection<ApplicationComponent> applicationComponents =
-            Lists.newArrayList(
-                    c.createApplicationComponent(appCompName, appName, lifecycleName, vmName, tasks),
-                    c.createApplicationComponent(appCompName2, appName, lifecycleName2, vmName2, tasks))
+    applicationComponents = Lists.newArrayList(
+            c.createApplicationComponent(appCompName, appName, lifecycleName, vmName, tasks),
+            c.createApplicationComponent(appCompName2, appName, lifecycleName2, vmName2, tasks))
 
-    Collection<ApplicationComponentInstance> applicationComponentInstances =
-            Lists.newArrayList(
-                    c.createApplicationComponentInstance(
-                            appCompInstName, appName, vmInstName, appCompName, tasks),
-                    c.createApplicationComponentInstance(
-                            appCompInstName2, appName, vmInstName2, appCompName2, tasks))
+    applicationComponentInstances = Lists.newArrayList(
+            c.createApplicationComponentInstance(
+                    appCompInstName, appName, vmInstName, appCompName, tasks),
+            c.createApplicationComponentInstance(
+                    appCompInstName2, appName, vmInstName2, appCompName2, tasks))
 
-    Collection<Communication> communications = new LinkedList<>()
-    Collection<PortProvided> portsProvided = new LinkedList<>()
-    Collection<PortRequired> portsRequired = new LinkedList<>()
-
-    Collection<VirtualMachineInstanceMonitor> virtualMachineInstanceMonitors =
-            Lists.newArrayList(
-                    c.toMonitor1(vmInstName, tasks),
-                    c.toMonitor1(vmInstName2, tasks))
-    Collection<ApplicationComponentInstanceMonitor> applicationComponentInstanceMonitors =
-            Lists.newArrayList(
-                    c.toMonitor3(appCompInstName, tasks),
-                    c.toMonitor3(appCompInstName2, tasks))
+    virtualMachineInstanceMonitors = Lists.newArrayList(
+            c.toMonitor1(vmInstName, tasks),
+            c.toMonitor1(vmInstName2, tasks))
+    applicationComponentInstanceMonitors = Lists.newArrayList(
+            c.toMonitor3(appCompInstName, tasks),
+            c.toMonitor3(appCompInstName2, tasks))
 
     ComparableModel model = createModel(
             cloudApis, clouds, cloudProperties, cloudCredentials,
@@ -188,7 +168,39 @@ class DefaultGraphGeneratorTests extends Specification {
             applicationComponentInstances, communications, portsProvided,
             portsRequired, virtualMachineInstanceMonitors,
             applicationComponentInstanceMonitors)
-    DefaultGraphGenerator generator = new DefaultGraphGenerator()
+
+
+    when:
+    SimpleDirectedGraph<Task, DefaultEdge> graph = generator.generateConfigGraph(model)
+
+    then:
+    noExceptionThrown()
+    checkGraph(graph, tasks, dependencies)
+
+  }
+
+  def "graph generation: only communication"() {
+
+    setup:
+    Application application = c.createApplication(appName, tasks)
+    ApplicationInstance applicationInstance = c.createApplicationInstance(appInstName, appName, tasks)
+
+    communications = Lists.newArrayList(c.createCommunication(
+            communicationName, portProvidedName, portRequiredName, tasks))
+    portsProvided = Lists.newArrayList(
+            c.createPortProvided(portProvidedName, appCompName, tasks),
+            c.createPortProvided(portProvidedName2, appCompName2, tasks))
+    portsRequired = Lists.newArrayList(
+            c.createPortRequired(portRequiredName, appCompName, tasks))
+
+
+    ComparableModel model = createModel(
+            cloudApis, clouds, cloudProperties, cloudCredentials,
+            application, applicationInstance, lifecycleComponents,
+            virtualMachines, virtualMachineInstances, applicationComponents,
+            applicationComponentInstances, communications, portsProvided,
+            portsRequired, virtualMachineInstanceMonitors,
+            applicationComponentInstanceMonitors)
 
     when:
     SimpleDirectedGraph<Task, DefaultEdge> graph = generator.generateConfigGraph(model)
@@ -256,6 +268,24 @@ class DefaultGraphGeneratorTests extends Specification {
   def portProvidedName2 = "testPortProvidedName_2"
   def portRequiredName = "testPortRequiredName"
   //def portRequiredName2 = "testPortRequiredName_2"
+
+  Collection<CloudApi> cloudApis = new LinkedList<>()
+  Collection<Cloud> clouds = new LinkedList<>()
+  Collection<CloudProperty> cloudProperties = new LinkedList<>()
+  Collection<CloudCredential> cloudCredentials = new LinkedList<>()
+  Collection<LifecycleComponent> lifecycleComponents = new LinkedList<>()
+  Collection<VirtualMachine> virtualMachines = new LinkedList<>()
+  Collection<VirtualMachineInstance> virtualMachineInstances = new LinkedList<>()
+  Collection<ApplicationComponent> applicationComponents = new LinkedList<>()
+  Collection<ApplicationComponentInstance> applicationComponentInstances = new LinkedList<>()
+  Collection<Communication> communications = new LinkedList<>()
+  Collection<PortProvided> portsProvided = new LinkedList<>()
+  Collection<PortRequired> portsRequired = new LinkedList<>()
+  Collection<VirtualMachineInstanceMonitor> virtualMachineInstanceMonitors = new LinkedList<>()
+  Collection<ApplicationComponentInstanceMonitor> applicationComponentInstanceMonitors= new LinkedList<>()
+
+
+
 
 
   boolean checkGraph(SimpleDirectedGraph<Task, DefaultEdge> graph,
