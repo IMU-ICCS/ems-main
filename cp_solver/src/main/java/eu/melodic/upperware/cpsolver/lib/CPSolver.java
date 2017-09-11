d17ace600e990e5f6de4e647c0391086a1b3ba66
mprusinski <mprusinski@7bulls.com>
2017-09-11 13:42:25 +0200

    test

--- cp_solver/src/main/java/eu/melodic/upperware/cpsolver/lib/CPSolver.java
+++ cp_solver/src/main/java/eu/melodic/upperware/cpsolver/lib/CPSolver.java
@@ -275,7 +275,7 @@ public class CPSolver {
 				log.info("2. Optimal value is: " + intGoal.getValue());
 			}
 			log.info("1. Checking if solver has solutions");
-			hasSolutions = (solver.isFeasible() == ESat.TRUE);
+			hasSolutions = true; //(solver.isFeasible() == ESat.TRUE);
 			log.info("1. Does solver has solutions? " + hasSolutions);
 			if (hasSolutions) saveSolution();
 			try{
