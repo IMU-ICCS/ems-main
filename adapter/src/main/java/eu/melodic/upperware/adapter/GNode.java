1e90a20bc7f7e530fb116817107324abf9ddc2d4
pszkup <pszkup@7bulls.com>
2017-10-16 12:13:45 +0200

    logging task graph

---
+++ adapter/src/main/java/eu/melodic/upperware/adapter/GNode.java
@@ -0,0 +1,29 @@
+package eu.melodic.upperware.adapter;
+
+import eu.melodic.upperware.adapter.plangenerator.tasks.Task;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by pszkup on 16.10.17.
+ */
+
+@Getter
+@Setter
+public class GNode {
+
+    public Task parent;
+
+    public List<Task> children = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n").append(this.getParent() != null ? this.getParent() + "@" + Integer.toHexString(hashCode()) : " --- ").append("\n");
+        this.getChildren().forEach(task -> sb.append("\t").append(task).append("@").append(Integer.toHexString(task.hashCode())).append("\n"));
+        return sb.toString();
+    }
+}
