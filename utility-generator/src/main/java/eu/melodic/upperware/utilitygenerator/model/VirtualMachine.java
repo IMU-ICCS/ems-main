493997ddc07366327138b61b981a05e733f94c29
mmaczkowska <mmaczkowska@7bulls.com>
2017-10-20 15:15:50 +0200

    change in virtual machine constructor

--- utility-generator/src/main/java/eu/melodic/upperware/utilitygenerator/model/VirtualMachine.java
+++ utility-generator/src/main/java/eu/melodic/upperware/utilitygenerator/model/VirtualMachine.java
@@ -1,15 +1,15 @@
 package eu.melodic.upperware.utilitygenerator.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class VirtualMachine {
 
-  private String id;
-  private Double cost;
+  private final String id;
+  private final Double cost;
   @Setter
   private int count;
 }
