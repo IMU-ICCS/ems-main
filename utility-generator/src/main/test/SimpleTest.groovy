af892854ca27faf04d985e3dea88d25da50c9150
mmaczkowska <mmaczkowska@7bulls.com>
2017-10-20 10:28:46 +0200

    fixes - normalization of beta distribution and adding new tests from document

--- utility-generator/src/main/test/SimpleTest.groovy
+++ utility-generator/src/main/test/SimpleTest.groovy
@@ -9,21 +9,59 @@ import spock.lang.Specification
 
 class SimpleTest extends Specification{
 
-  VirtualMachine vm1
+  VirtualMachine vm1_1inst
+  VirtualMachine vm1_2inst
+  VirtualMachine vm1_3inst
   VirtualMachine vm2
   Collection<VirtualMachine> initialConfiguration
-  Collection<VirtualMachine> newConfiguration
+  Collection<VirtualMachine> newConfigurationWithLessMachines
+  Collection<VirtualMachine> newConfigurationWithMoreMachines
   UtilityFunctionEvaluator evaluator
   Map<MetricType, Metric> metrics
 
   def setup(){
-    vm1 = new VirtualMachine("mBig", 3, 2)
+    vm1_1inst = new VirtualMachine("mBig", 3, 1)
+    vm1_2inst = new VirtualMachine("mBig", 3, 2)
+    vm1_3inst = new VirtualMachine("mBig", 3, 3)
+
     vm2 = new VirtualMachine("mXXL", 10, 0)
-    initialConfiguration = Lists.newArrayList(vm1, vm2)
+    initialConfiguration = Lists.newArrayList(vm1_2inst, vm2)
+
     metrics = new HashMap<>()
     metrics.put(MetricType.MAX_RESPONSE_TIME, new Metric(MetricType.MAX_RESPONSE_TIME, 30))
     metrics.put(MetricType.NOM_RESPONSE_TIME, new Metric(MetricType.NOM_RESPONSE_TIME, 20))
     metrics.put(MetricType.COST_WEIGHT, new Metric(MetricType.COST_WEIGHT, 0.5))
+
+    newConfigurationWithLessMachines = Lists.newArrayList(vm1_1inst, vm2)
+    newConfigurationWithMoreMachines = Lists.newArrayList(vm1_3inst, vm2)
+  }
+
+  def "avg response time=3, more machines"(){
+    setup:
+    metrics.put(MetricType.AVG_RESPONSE_TIME, new Metric(MetricType.AVG_RESPONSE_TIME, 3))
+    evaluator = new UtilityFunctionEvaluatorExample(initialConfiguration, metrics)
+
+
+
+    when:
+    double result = evaluator.evaluate(newConfigurationWithMoreMachines)
+
+    then:
+    //Out[40]= 0.33334
+    result >= 0.28 && result <= 0.38
+  }
+
+  def "avg response time=3, less machines"(){
+    setup:
+    metrics.put(MetricType.AVG_RESPONSE_TIME, new Metric(MetricType.AVG_RESPONSE_TIME, 3))
+    evaluator = new UtilityFunctionEvaluatorExample(initialConfiguration, metrics)
+
+    when:
+    double result = evaluator.evaluate(newConfigurationWithLessMachines)
+
+    then:
+    //0.503253
+    result >= 0.45 && result <= 0.55
   }
 
   def "avg response time=3, no changes"(){
@@ -35,38 +73,136 @@ class SimpleTest extends Specification{
     double result = evaluator.evaluate(initialConfiguration)
 
     then:
-    System.out.println("result = " + result)
+    //0.500068
+    result >= 0.45 && result <= 0.55
   }
 
-  def "avg response time=3, more machines"(){
+  def "avg response time=25, more machines"(){
     setup:
-    metrics.put(MetricType.AVG_RESPONSE_TIME, new Metric(MetricType.AVG_RESPONSE_TIME, 3))
+    metrics.put(MetricType.AVG_RESPONSE_TIME, new Metric(MetricType.AVG_RESPONSE_TIME, 25))
     evaluator = new UtilityFunctionEvaluatorExample(initialConfiguration, metrics)
 
-    VirtualMachine vm1_1 = new VirtualMachine("mBig", 3, 3)
-    VirtualMachine vm2_2 = new VirtualMachine("mXXL", 10, 0)
-    newConfiguration = Lists.newArrayList(vm1_1, vm2_2)
-
     when:
-    double result = evaluator.evaluate(newConfiguration)
+    double result = evaluator.evaluate(newConfigurationWithMoreMachines)
 
     then:
-    System.out.println("result = " + result)
+    //0.677073
+    result >= 0.62 && result <= 0.72
   }
 
-  def "avg response time=3, less machines"(){
+  def "avg response time=25, less machines"(){
     setup:
-    metrics.put(MetricType.AVG_RESPONSE_TIME, new Metric(MetricType.AVG_RESPONSE_TIME, 3))
+    metrics.put(MetricType.AVG_RESPONSE_TIME, new Metric(MetricType.AVG_RESPONSE_TIME, 25))
     evaluator = new UtilityFunctionEvaluatorExample(initialConfiguration, metrics)
 
-    VirtualMachine vm1_1 = new VirtualMachine("mBig", 3, 1)
-    VirtualMachine vm2_2 = new VirtualMachine("mXXL", 10, 0)
-    newConfiguration = Lists.newArrayList(vm1_1, vm2_2)
-
     when:
-    double result = evaluator.evaluate(newConfiguration)
+    double result = evaluator.evaluate(newConfigurationWithLessMachines)
 
     then:
-    System.out.println("result = " + result)
+    //0.5
+    result >= 0.45 && result <= 0.55
+
   }
+  def "avg response time=25, no changes"(){
+    setup:
+    metrics.put(MetricType.AVG_RESPONSE_TIME, new Metric(MetricType.AVG_RESPONSE_TIME, 25))
+    evaluator = new UtilityFunctionEvaluatorExample(initialConfiguration, metrics)
+
+    when:
+    double result = evaluator.evaluate(initialConfiguration)
+
+    then:
+    //0.837175
+    result >= 0.77 && result <= 0.87
+  }
+
+  def "avg response time=28, more machines"(){
+    setup:
+    metrics.put(MetricType.AVG_RESPONSE_TIME, new Metric(MetricType.AVG_RESPONSE_TIME, 28))
+    evaluator = new UtilityFunctionEvaluatorExample(initialConfiguration, metrics)
+
+    when:
+    double result = evaluator.evaluate(newConfigurationWithMoreMachines)
+
+    then:
+    //0.785278
+    result >= 0.73 && result <= 0.83
+  }
+
+  def "avg response time=28, less machines"(){
+    setup:
+    metrics.put(MetricType.AVG_RESPONSE_TIME, new Metric(MetricType.AVG_RESPONSE_TIME, 28))
+    evaluator = new UtilityFunctionEvaluatorExample(initialConfiguration, metrics)
+
+    when:
+    double result = evaluator.evaluate(newConfigurationWithLessMachines)
+
+    then:
+    //0.5
+    result >= 0.45 && result <= 0.55
+
+  }
+  def "avg response time=28, no changes"(){
+    setup:
+    metrics.put(MetricType.AVG_RESPONSE_TIME, new Metric(MetricType.AVG_RESPONSE_TIME, 28))
+    evaluator = new UtilityFunctionEvaluatorExample(initialConfiguration, metrics)
+
+    when:
+    double result = evaluator.evaluate(initialConfiguration)
+
+    then:
+    //0.567346
+    System.out.println(result)
+
+    result >= 0.51 && result <= 0.61
+  }
+
+  def "avg response time=25, zeta = 0.05, more machines"(){
+    setup:
+    metrics.put(MetricType.AVG_RESPONSE_TIME, new Metric(MetricType.AVG_RESPONSE_TIME, 25))
+    metrics.put(MetricType.COST_WEIGHT, new Metric(MetricType.COST_WEIGHT, 0.05))
+    evaluator = new UtilityFunctionEvaluatorExample(initialConfiguration, metrics)
+
+    when:
+    double result = evaluator.evaluate(newConfigurationWithMoreMachines)
+
+    then:
+    //0.686438
+    System.out.println(result)
+
+    result >= 0.63 && result <= 0.73
+  }
+
+  def "avg response time=25, zeta = 0.05, less machines"(){
+    setup:
+    metrics.put(MetricType.AVG_RESPONSE_TIME, new Metric(MetricType.AVG_RESPONSE_TIME, 25))
+    metrics.put(MetricType.COST_WEIGHT, new Metric(MetricType.COST_WEIGHT, 0.05))
+    evaluator = new UtilityFunctionEvaluatorExample(initialConfiguration, metrics)
+
+    when:
+    double result = evaluator.evaluate(newConfigurationWithLessMachines)
+
+    then:
+    //0.05
+    System.out.println(result)
+
+    result >= 0.0 && result <= 0.1
+
+  }
+  def "avg response time=25, zeta = 0.05, no changes"(){
+    setup:
+    metrics.put(MetricType.AVG_RESPONSE_TIME, new Metric(MetricType.AVG_RESPONSE_TIME, 25))
+    metrics.put(MetricType.COST_WEIGHT, new Metric(MetricType.COST_WEIGHT, 0.05))
+    evaluator = new UtilityFunctionEvaluatorExample(initialConfiguration, metrics)
+
+    when:
+    double result = evaluator.evaluate(initialConfiguration)
+
+    then:
+    //0.690632
+    System.out.println(result)
+    result >= 0.64 && result <= 0.74
+  }
+
+
 }
