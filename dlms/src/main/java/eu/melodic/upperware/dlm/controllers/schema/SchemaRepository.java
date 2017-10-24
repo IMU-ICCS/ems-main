4640090e92be24d635d99c4f4155068d39d104a7
Somnath <mazumdar@simula.no>
2017-10-24 16:54:26 +0200

    Initial commit_v1

--- dlms/src/main/java/eu/melodic/upperware/dlm/controllers/schema/SchemaRepository.java
+++
@@ -1,8 +0,0 @@
-package eu.melodic.upperware.dlm.controllers.schema;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface SchemaRepository extends CrudRepository<DbSchema, Long> {
-
-}
