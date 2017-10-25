339ce3c51e62fc768cd4e7b05b1b079025959db2
mprusinski <mprusinski@7bulls.com>
2017-10-25 13:39:08 +0200

    added examples how to use integration artifacts (interfaces generated from integraion project from RAML files)

--- dlms/src/main/java/eu/melodic/upperware/dlms/controllers/DataCostDBController.java
+++ dlms/src/main/java/eu/melodic/upperware/dlms/controllers/DataCostDBController.java
@@ -17,8 +17,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import eu.melodic.models.interfaces.dlms.*;
 
 import eu.melodic.upperware.dlms.store.DataCostHistory;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 // TODO: I will save the hardcoded daat into the mysql as history of the related to the daat moveemnt
 
@@ -27,6 +32,34 @@ public class DataCostDBController {
 
 	@Autowired
 	private DataCostService dataCostService;
+
+	@RequestMapping(value = "/dataPool/transferCost", method = GET, produces = APPLICATION_JSON_VALUE)
+	public DataPoolTransferCostResponse transferCost() {
+		//TODO: do the logic and build reponse object
+		DataPoolTransferCostResponseImpl response = new DataPoolTransferCostResponseImpl();
+		response.setDataPoolId("somePoolId");
+		response.setCost("500");
+
+		return response;
+
+	}
+
+	@RequestMapping(value = "/dataPool", method = POST, produces = APPLICATION_JSON_VALUE)
+	public void transferCost(@RequestBody DataPoolRequestImpl request) {
+		//retrieve field's values and do some logic
+
+		System.out.println("Received POST dataPool request with data: " +request.getDataPoolId() +":" +request.getVolume());
+		//TODO: do the logic
+	}
+
+	@RequestMapping(value = "/update", method = POST, produces = APPLICATION_JSON_VALUE)
+	public void transferCost(@RequestBody DataPoolUpdateRequestImpl request) {
+		//retrieve field's values and do some logic
+		System.out.println("Received POST dataPool UPDATE request with data: " +request.getDataPoolId() +":" +request.getVolume());
+		//TODO: do the logic
+	}
+
+
 
 	@RequestMapping("/cost")
 	public List<DataCostHistory> getAllCosts() {
