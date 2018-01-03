57035fcb663e5118781e0e7180b5798b87351714
mmaczkowska <mmaczkowska@7bulls.com>
2018-01-03 14:59:50 +0100

    fix pom

---
+++ utility-generator/src/main/java/eu/melodic/upperware/utilitygenerator/costfunction/CostUtilityFunction.java
@@ -0,0 +1,26 @@
+/* * Copyright (C) 2017 7bulls.com
+*
+* This Source Code Form is subject to the terms of the
+* Mozilla Public License, v. 2.0. If a copy of the MPL
+* was not distributed with this file, You can obtain one at
+* http://mozilla.org/MPL/2.0/.
+*/
+
+package eu.melodic.upperware.utilitygenerator.costfunction;
+
+import eu.melodic.upperware.utilitygenerator.model.Component;
+
+import java.util.Collection;
+
+public abstract class CostUtilityFunction {
+
+
+  public abstract double evaluateCostUtilityFunction(Collection<Component> actualConfiguration,
+    Collection<Component> newConfiguration);
+
+  double calculateCost(Collection<Component> vms){
+
+    return vms.stream().mapToDouble(c -> c.getCardinality() * c.getNodeCandidate().getPrice()).sum();
+
+  }
+}
