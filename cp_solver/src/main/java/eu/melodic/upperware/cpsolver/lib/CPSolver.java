edd72ac164be2ca8ede73583ba353ddab1682f13
Marta Różańska <mrozanska@7bulls.com>
2018-02-20 10:47:02 +0100

    restoration of commented code

--- cp_solver/src/main/java/eu/melodic/upperware/cpsolver/lib/CPSolver.java
+++ cp_solver/src/main/java/eu/melodic/upperware/cpsolver/lib/CPSolver.java
@@ -401,9 +401,20 @@ public class CPSolver {
 		if (isReconfig) {
 			updateUtilityOfDeployedSolution(cp);
 		}
-		Solution solution = CpFactory.eINSTANCE.createSolution();
-		solution.setTimestamp(new Date().getTime());
-		cp.getSolution().add(solution);
+
+        Solution solution = null;
+        //if (timestamp == 0){
+        solution = CpFactory.eINSTANCE.createSolution();
+        solution.setTimestamp(new Date().getTime());
+        cp.getSolution().add(solution);
+//		} else {
+//			for (Solution s: cp.getSolution()){
+//				if (s.getTimestamp() == timestamp){
+//					solution = s;
+//					break;
+//				}
+//			}
+//		}
 
 		DoubleValueUpperware utilityValue = TypesFactory.eINSTANCE.createDoubleValueUpperware();
 		utilityValue.setValue(maxUtility);
