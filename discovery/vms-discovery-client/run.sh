68864f83d2c7768626af97ff0883923b4279ce98
ipatini <ipatini@acci.gr>
2018-02-26 15:29:58 +0200

    vms, script improvement

--- discovery/vms-discovery-client/run.sh
+++ discovery/vms-discovery-client/run.sh
@@ -6,12 +6,23 @@ PWD=$( pwd )
 cd $BASEDIR
 
 # Append VMS server credentials to client.properties file
-VMSCRED=`cat target/classes/client.properties | grep username | wc -l`
-#echo $VMSCRED
-if [ $VMSCRED == '0' ]; then
-  echo 'Missing VMS Discovery Server credentials...'
-  sudo /bin/bash -c "cat ../vms-server.credentials >> target/classes/client.properties"
-  echo 'Credentials appended to target/classes/client.properties'
+if [[ -f 'target/classes/client.properties' ]]; then
+	VMSCRED=`cat target/classes/client.properties | grep username | wc -l`
+	#echo $VMSCRED
+	if [ $VMSCRED == '0' ]; then
+	  echo 'Missing VMS Discovery Server credentials...'
+	  sudo /bin/bash -c "cat ../vms-server.credentials >> target/classes/client.properties"
+	  echo 'Credentials appended to target/classes/client.properties'
+	fi
+fi
+if [[ -f 'conf/client.properties' ]]; then
+	VMSCRED=`cat conf/client.properties | grep username | wc -l`
+	#echo $VMSCRED
+	if [ $VMSCRED == '0' ]; then
+	  echo 'Missing VMS Discovery Server credentials...'
+	  sudo /bin/bash -c "cat ../vms-server.credentials >> conf/client.properties"
+	  echo 'Credentials appended to conf/client.properties'
+	fi
 fi
 
 # Run VMS client
