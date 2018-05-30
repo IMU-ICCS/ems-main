22fa270df1366c5de239c8440f720a75d0cee754
Marcin Prusiński <mprusinski@7bulls.com>
2018-05-30 14:42:37 +0200

    added Esper for CAS use-case to the discovery server

--- discovery/vms-discovery-server/start-level-3.sh
+++ discovery/vms-discovery-server/start-level-3.sh
@@ -25,6 +25,13 @@ JAVA_OPTS="-Xms256m -Xmx1024m -XX:PermSize=512m -XX:MaxPermSize=512m"
     echo 'Copying Mule App for Genom completed. Starting mule...'
     ./mule start "-M-Dhost.rulefirst=select\ ComputeMetricValue\(metricValue,\ 0.95\)\ as\ metricValue,\  vmName\ as\ vmName,\ cloudName\ as\ cloudName,\ componentName\ as\ componentName,\ 2\ as\ level,\ current_timestamp\ as\ timestamp\  from\ Visor_event_second.win:length\(\6\)\ output\ last\ every\ 6\ events" "-M-Dhost.rulesecond=insert\  into\ CombinedEvent\(\metricValue\)\ select\ \(\(\Math.ceil\(\A.metricValue\ /\ D.metricValue\)\ *\ B.metricValue\)\ -\ C.metricValue\)\  as\ metricValue\  from\ Visor_event.win:length_batch\(1\)\ A,\ Visor_event_second.win:length_batch\(1\)\ B,\ Visor_event_third.win:length_batch\(1\)\ C,\ Visor_event_fourth.win:length_batch\(1\)\ D\  output\ last\ every\ 10\ sec" "-M-Dhost.rulethird=insert\ into\ MinimumCoresEvent\(\metricValue\)\ select\ \(\Math.ceil\(\A.metricValue\ /\ \Math.floor\(\C.metricValue\ /\ B.metricValue\)\)\)\  as\ metricValue\  from\ Visor_event.win:length_batch\(1\)\ A,\ Visor_event_second.win:length_batch\(1\)\ B,\ Visor_event_third.win:length_batch\(1\)\ C\  output\ last\ every\ 10\ sec"  "-M-Dhost.rulefourth=select\ *\ from\ Visor_event_fifth.win:time\(20\ sec\)\ having\ metricValue\>\0\  output\ last\ every\ 20\ sec"
     echo 'Mule started.'
+  elif [ "$USECASE" == "CAS" ]
+  then
+    echo 'Copying Mule App for CAS App...'
+    cp /opt/CAS_app.zip /opt/mule-standalone-3.9.0/apps
+    echo 'Copying Mule App for CAS completed. Starting mule...'
+    ./mule start "-M-Dhost.rulescaleout=select\ avg\(metricValue\)\ as\ metricValue,\ vmName\ as\ vmName,\ cloudName\ as\ cloudName,\ componentName\ as\ componentName,\ 2\ as\ level,\ current_timestamp\ as\ timestamp\ from\ Visor_event.win:time\(60\ sec\)\ having\ avg\(\metricValue\)\>80\ output\ last\ every\ 120\ sec" "-M-Dhost.rulescalein=select\ avg\(metricValue\)\ as\ metricValue,\ vmName\ as\ vmName,\ cloudName\ as\ cloudName,\ componentName\ as\ componentName,\ 2\ as\ level,\ current_timestamp\ as\ timestamp\ from\ Visor_event.win:time\(60\ sec\)\ having\ avg\(\metricValue\)\<\=30\ output\ last\ every\ 120\ sec"
+    echo 'Mule started.'
   else
     echo "Unsupported use-case: $USECASE"
   fi
