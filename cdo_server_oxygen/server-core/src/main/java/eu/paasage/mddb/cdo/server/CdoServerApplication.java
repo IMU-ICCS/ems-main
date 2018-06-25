79d54103f05723c824ef2e31ed597de6bb517718
pszkup <pszkup@7bulls.com>
2018-06-25 11:38:32 +0200

    cdo server with camel oxygen

---
+++ cdo_server_oxygen/server-core/src/main/java/eu/paasage/mddb/cdo/server/CdoServerApplication.java
@@ -0,0 +1,12 @@
+package eu.paasage.mddb.cdo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdoServerApplication.class, args);
+    }
+}
