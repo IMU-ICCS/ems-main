40d8d600d34a85fc54f4f7f13389353f81a510ad
Kyriakos Kritikos <kritikos@ics.forth.gr>
2018-07-22 16:00:37 +0300

    Correcting validation issues + updating java examples (SensApp, Organisation Model)

--- cdo_client_oxygen/src/main/java/eu/paasage/mddb/cdo/client/OCLValidation.java
+++ cdo_client_oxygen/src/main/java/eu/paasage/mddb/cdo/client/OCLValidation.java
@@ -9,7 +9,15 @@ package eu.paasage.mddb.cdo.client;
 
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.impl.EPackageRegistryImpl;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.Diagnostician;
+import org.eclipse.ocl.pivot.internal.resource.StandaloneProjectMap;
+import org.eclipse.ocl.pivot.internal.utilities.PivotEnvironmentFactory;
+import org.eclipse.ocl.pivot.oclstdlib.OCLstdlibPackage;
 
 
 public class OCLValidation {
@@ -24,21 +32,21 @@ public class OCLValidation {
 	}
 	
 	private static void initOcl(){
-//		System.setProperty("javax.xml.parsers.DocumentBuilderFactory", "com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl");
-//		final ResourceSet resourceSet =new ResourceSetImpl();
-//		org.eclipse.ocl.uml.OCL.initialize(resourceSet);
-//		org.eclipse.ocl.pivot.model.OCLstdlib.install();
-//		org.eclipse.ocl.ecore.delegate.OCLDelegateDomain.initialize(resourceSet);
-//		org.eclipse.ocl.xtext.completeocl.CompleteOCLStandaloneSetup.doSetup();
-//		org.eclipse.ocl.xtext.oclinecore.OCLinEcoreStandaloneSetup.doSetup();
-//		org.eclipse.ocl.xtext.oclstdlib.OCLstdlibStandaloneSetup.doSetup();
-//		StandaloneProjectMap map = StandaloneProjectMap.getAdapter(resourceSet);
-//
-//		EPackage.Registry registry = new EPackageRegistryImpl();
-//		registry.put(OCLstdlibPackage.eNS_URI, OCLstdlibPackage.eINSTANCE);
-//		registry.put(EcorePackage.eNS_URI, EcorePackage.eINSTANCE);
-//		PivotEnvironmentFactory environmentFactory = new PivotEnvironmentFactory(map,resourceSet);
-//		org.eclipse.ocl.pivot.utilities.OCL ocl = org.eclipse.ocl.pivot.utilities.OCL.newInstance(environmentFactory);
+		System.setProperty("javax.xml.parsers.DocumentBuilderFactory", "com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl");
+		final ResourceSet resourceSet =new ResourceSetImpl();
+		org.eclipse.ocl.uml.OCL.initialize(resourceSet);
+		org.eclipse.ocl.pivot.model.OCLstdlib.install();
+		org.eclipse.ocl.ecore.delegate.OCLDelegateDomain.initialize(resourceSet);
+		org.eclipse.ocl.xtext.completeocl.CompleteOCLStandaloneSetup.doSetup();
+		org.eclipse.ocl.xtext.oclinecore.OCLinEcoreStandaloneSetup.doSetup();
+		org.eclipse.ocl.xtext.oclstdlib.OCLstdlibStandaloneSetup.doSetup();
+		StandaloneProjectMap map = StandaloneProjectMap.getAdapter(resourceSet);
+
+		EPackage.Registry registry = new EPackageRegistryImpl();
+		registry.put(OCLstdlibPackage.eNS_URI, OCLstdlibPackage.eINSTANCE);
+		registry.put(EcorePackage.eNS_URI, EcorePackage.eINSTANCE);
+		PivotEnvironmentFactory environmentFactory = new PivotEnvironmentFactory(map,resourceSet);
+		org.eclipse.ocl.pivot.utilities.OCL ocl = org.eclipse.ocl.pivot.utilities.OCL.newInstance(environmentFactory);
 	}
 	
 	public static boolean validate(EObject obj) throws RuntimeException{
@@ -46,6 +54,7 @@ public class OCLValidation {
 		Diagnostic diagnostic = Diagnostician.INSTANCE.validate(obj);
 		if (diagnostic.getSeverity() != Diagnostic.OK) {
 			log.error("VALIDATION ERROR: " + diagnostic);
+			//System.out.println("VALIDATION ERROR: " + diagnostic);
 			return false;
 		}
 		return true;
