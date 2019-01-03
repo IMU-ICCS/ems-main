96ac91bb283379222a2453d6aef1d57cedd22564
ipatini <ipatini@acci.gr>
2019-01-03 09:59:58 +0200

    EMS - Cleanup of pom's (removed unused parts). Renamed baguette client assembly file (from docker to vm). In control service pom.xml, added plugin for copying Esper jar in 'target' directory.

--- event-management/run.bat
+++ event-management/run.bat
@@ -5,12 +5,17 @@ set curdir=%~dp0
 set MELODIC_CONFIG_DIR=%curdir%\config-files
 set PAASAGE_CONFIG_DIR=%curdir%\config-files
 
-rem set JAVA_OPTS=-Djavax.net.debug=all 
-rem java %JAVA_OPTS% -jar control-service\target\control-service.jar --logging.config=%MELODIC_CONFIG_DIR%\logback-spring.xml
-java %JAVA_OPTS% -cp control-service\target\control-service.jar -Dloader.path=control-service\esper-7.1.0.jar org.springframework.boot.loader.PropertiesLauncher --logging.config=%MELODIC_CONFIG_DIR%\logback-spring.xml
+rem Uncomment next line to set JAVA runtime options
+rem set JAVA_OPTS=-Djavax.net.debug=all
+
+rem Uncomment next line to run a Broker-CEP test scenario (BrokerCepServiceTest1..BrokerCepServiceTest5)
+rem set BROKER_CEP_TEST=-Drun-broker-cep-test=BrokerCepServiceTest5
+
+rem Use when Esper is packaged in control-service.jar
+rem java %JAVA_OPTS% -jar %BROKER_CEP_TEST% control-service\target\control-service.jar --logging.config=%MELODIC_CONFIG_DIR%\logback-spring.xml
+rem Use when Esper is NOT packaged in control-service.jar
+java %JAVA_OPTS% -cp control-service\target\control-service.jar -Dloader.path=control-service\target\esper-7.1.0.jar %BROKER_CEP_TEST% org.springframework.boot.loader.PropertiesLauncher --logging.config=%MELODIC_CONFIG_DIR%\logback-spring.xml
 rem e.g. --spring.config.location=%MELODIC_CONFIG_DIR%\
 rem e.g. --spring.config.name=application.properties
 
-rem Uncomment next line (and comment out the previous one) to run a Broker-CEP tests scenario (BrokerCepServiceTest1..BrokerCepServiceTest5)
-rem java -jar -Drun-broker-cep-test=BrokerCepServiceTest5 control-service\target\control-service.jar --logging.config=%MELODIC_CONFIG_DIR%\logback-spring.xml
 endlocal
