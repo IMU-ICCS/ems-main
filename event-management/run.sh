d48dc3fee71cbc54eedc00a672b7583e4203b5c8
ipatini <ipatini@acci.gr>
2019-01-08 20:42:13 +0200

    EMS - Updated EMS launch script for linux

--- event-management/run.sh
+++ event-management/run.sh
@@ -5,14 +5,18 @@ BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 MELODIC_CONFIG_DIR=$BASEDIR/config-files
 PAASAGE_CONFIG_DIR=$BASEDIR/config-files
 
+export MELODIC_CONFIG_DIR PAASAGE_CONFIG_DIR
+
 # Uncomment next line to set JAVA runtime options
 # JAVA_OPTS=-Djavax.net.debug=all
 
 # Uncomment next line to run a Broker-CEP test scenario (BrokerCepServiceTest1..BrokerCepServiceTest5)
 # BROKER_CEP_TEST=-Drun-broker-cep-test=BrokerCepServiceTest5
 
+export JAVA_OPTS BROKER_CEP_TEST
+
 # Use when Esper is packaged in control-service.jar
-# java %JAVA_OPTS% -jar %BROKER_CEP_TEST% control-service\target\control-service.jar --logging.config=%MELODIC_CONFIG_DIR%\logback-spring.xml
+# java $JAVA_OPTS -jar $BROKER_CEP_TEST $BASEDIR/control-service/target/control-service.jar --logging.config=$MELODIC_CONFIG_DIR/logback-spring.xml
 
 # Use when Esper is NOT packaged in control-service.jar
 java $JAVA_OPTS -cp $BASEDIR/control-service/target/control-service.jar -Dloader.path=$BASEDIR/control-service/target/esper-7.1.0.jar $BROKER_CEP_TEST org.springframework.boot.loader.PropertiesLauncher --logging.config=$MELODIC_CONFIG_DIR/logback-spring.xml
