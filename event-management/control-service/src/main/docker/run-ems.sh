8efda28426e908f03bd86829d09988bfa8f29c94
Marcin Prusiński <mprusinski@7bulls.com>
2019-01-15 13:32:59 +0100

    dockerfile change

--- event-management/control-service/src/main/docker/run-ems.sh
+++ event-management/control-service/src/main/docker/run-ems.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-MELODIC_CONFIG_DIR=$BASEDIR/config
-PAASAGE_CONFIG_DIR=$BASEDIR/config
+#BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+#MELODIC_CONFIG_DIR=/config
+#PAASAGE_CONFIG_DIR=/config
 
-export BASEDIR MELODIC_CONFIG_DIR PAASAGE_CONFIG_DIR
+#export BASEDIR MELODIC_CONFIG_DIR PAASAGE_CONFIG_DIR
 
-java -Duser.timezone=Europe/Warsaw -Djava.security.egd=file:/dev/./urandom -Dloader.path=$BASEDIR/esper-7.1.0.jar -cp $BASEDIR/control-service.jar org.springframework.boot.loader.PropertiesLauncher --logging.config=$MELODIC_CONFIG_DIR/logback-spring.xml
+$MELODIC_CONFIG_DIR/wait-for-cdo.sh && java -Duser.timezone=Europe/Warsaw -Djava.security.egd=file:/dev/./urandom -Dloader.path=esper-7.1.0.jar -cp control-service.jar org.springframework.boot.loader.PropertiesLauncher --logging.config=$MELODIC_CONFIG_DIR/logback-conf/logback-ems.xml
