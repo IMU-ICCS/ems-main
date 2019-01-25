fb3d832dc76b4160e2ff7762393c7d177c7c758c
ipatini <ipatini@acci.gr>
2019-01-25 10:37:44 +0100

    EMS - Baguette Client - Removed output redirection to file from 'install.sh' script (previous script has been renamed to install_WITH_LOGGING.sh)

--- event-management/baguette-client/bin/install_WITH_LOGGING.sh
+++ event-management/baguette-client/bin/install_WITH_LOGGING.sh
@@ -10,24 +10,31 @@
 
 # Create installation directories
 BIN_DIRECTORY=/opt/baguette-client/bin
-LOGS_DIRECTORY=/opt/baguette-client/conf
 LOGS_DIRECTORY=/opt/baguette-client/logs
 BASE_URL=$1
 APIKEY=$2
 
 mkdir -p $BIN_DIRECTORY/
-mkdir -p $CONF_DIRECTORY/
 mkdir -p $LOGS_DIRECTORY/
 
+# Create and change permissions of 'install-log.sh' script
+touch $BIN_DIRECTORY/install-log.sh
+chmod u=rwx,og-rwx $BIN_DIRECTORY/install-log.sh
+
+# Write contents of 'install-log.sh' script
+cat |sed -E 's/~/`/g' > $BIN_DIRECTORY/install-log.sh <<- EOM
+#!/usr/bin/env bash
+
 echo ""
-echo "** EMS Baguette Client - MELODIC PROJECT FP7 EU ...... **"
+echo "** Baguette Client - MELODIC PROJECT FP7 EU ...... **"
 echo "** Copyright ICCS-NTUA (C) 2016-2019, http://imu.iccs.gr **"
-echo ""
 date -Iseconds
 
 # Common variables
-DOWNLOAD_URL=$BASE_URL/baguette-client.zip
-DOWNLOAD_URL_MD5=$BASE_URL/baguette-client.zip.md5
+BASE_URL=\$1
+APIKEY=?ems-api-key=\$2
+DOWNLOAD_URL=\$BASE_URL/baguette-client.zip
+DOWNLOAD_URL_MD5=\$BASE_URL/baguette-client.zip.md5
 INSTALL_PACKAGE=/opt/baguette-client/baguette-client.zip
 INSTALL_PACKAGE_MD5=/opt/baguette-client/baguette-client.zip.md5
 INSTALL_DIR=/opt/
@@ -41,9 +48,9 @@ CREDENTIALS_FILE=/opt/baguette-client/conf/baguette-server.credentials
 echo ""
 echo "Create installation directory..."
 date -Iseconds
-mkdir -p $INSTALL_DIR/baguette-client
-if [ $? != 0 ]; then
-  echo "Failed to create installation directory ($?)"
+mkdir -p \$INSTALL_DIR/baguette-client
+if [ \$? != 0 ]; then
+  echo "Failed to create installation directory (\$?)"
   echo "Aborting installation..."
   date -Iseconds
   exit 1
@@ -53,9 +60,9 @@ fi
 echo ""
 echo "Download installation package..."
 date -Iseconds
-wget $DOWNLOAD_URL -O $INSTALL_PACKAGE
-if [ $? != 0 ]; then
-  echo "Failed to download installation package ($?)"
+wget \$DOWNLOAD_URL -O \$INSTALL_PACKAGE
+if [ \$? != 0 ]; then
+  echo "Failed to download installation package (\$?)"
   echo "Aborting installation..."
   date -Iseconds
   exit 1
@@ -67,9 +74,9 @@ echo "Download installation package...ok"
 echo ""
 echo "Download installation package MD5 checksum..."
 date -Iseconds
-wget $DOWNLOAD_URL_MD5 -O $INSTALL_PACKAGE_MD5
-if [ $? != 0 ]; then
-  echo "Failed to download installation package ($?)"
+wget \$DOWNLOAD_URL_MD5 -O \$INSTALL_PACKAGE_MD5
+if [ \$? != 0 ]; then
+  echo "Failed to download installation package (\$?)"
   echo "Aborting installation..."
   date -Iseconds
   exit 1
@@ -78,12 +85,12 @@ date -Iseconds
 echo "Download installation package MD5 checksum...ok"
 
 # Check MD5 checksum
-PACKAGE_MD5=`cat $INSTALL_PACKAGE_MD5`
-ZIP_CHECKSUM=`md5sum $INSTALL_PACKAGE |cut -d " " -f 1`
+PACKAGE_MD5=~cat \$INSTALL_PACKAGE_MD5~
+ZIP_CHECKSUM=~md5sum \$INSTALL_PACKAGE |cut -d " " -f 1~
 echo ""
-echo "Checksum MD5:  $PACKAGE_MD5"
-echo "Checksum calc: $ZIP_CHECKSUM"
-if [ $ZIP_CHECKSUM == $PACKAGE_MD5 ]; then
+echo "Checksum MD5:  \$PACKAGE_MD5"
+echo "Checksum calc: \$ZIP_CHECKSUM"
+if [ \$ZIP_CHECKSUM == \$PACKAGE_MD5 ]; then
   echo "Checksum: ok"
 else
   echo "Checksum: wrong"
@@ -96,9 +103,9 @@ fi
 echo ""
 echo "Unzip installation package..."
 date -Iseconds
-unzip -o $INSTALL_PACKAGE -d $INSTALL_DIR
-if [ $? != 0 ]; then
-  echo "Failed to unzip installation package ($?)"
+unzip -o \$INSTALL_PACKAGE -d \$INSTALL_DIR
+if [ \$? != 0 ]; then
+  echo "Failed to unzip installation package (\$?)"
   echo "Aborting installation..."
   date -Iseconds
   exit 1
@@ -109,9 +116,9 @@ date -Iseconds
 echo ""
 echo "Make scripts executable..."
 date -Iseconds
-chmod u=rx,og-rwx $INSTALL_DIR/baguette-client/bin/*
-if [ $? != 0 ]; then
-  echo "Failed to copy service script to /etc/init.d/ directory ($?)"
+chmod u=rx,og-rwx \$INSTALL_DIR/baguette-client/bin/*
+if [ \$? != 0 ]; then
+  echo "Failed to copy service script to /etc/init.d/ directory (\$?)"
   echo "Aborting installation..."
   date -Iseconds
   exit 1
@@ -121,17 +128,17 @@ fi
 echo ""
 echo "Register as a service..."
 date -Iseconds
-cp -f $STARTUP_SCRIPT /etc/init.d/
-if [ $? != 0 ]; then
-  echo "Failed to copy service script to /etc/init.d/ directory ($?)"
+cp -f \$STARTUP_SCRIPT /etc/init.d/
+if [ \$? != 0 ]; then
+  echo "Failed to copy service script to /etc/init.d/ directory (\$?)"
   echo "Aborting installation..."
   date -Iseconds
   exit 1
 fi
 
-update-rc.d $SERVICE_NAME defaults
-if [ $? != 0 ]; then
-  echo "Failed to register service script to /etc/init.d/ directory ($?)"
+update-rc.d \$SERVICE_NAME defaults
+if [ \$? != 0 ]; then
+  echo "Failed to register service script to /etc/init.d/ directory (\$?)"
   echo "Aborting installation..."
   date -Iseconds
   exit 1
@@ -140,18 +147,18 @@ fi
 # Add Id, Credentials and Client configuration files
 echo "Add Id, Credentials and Client configuration files"
 date -Iseconds
-touch $CLIENT_ID_FILE $CREDENTIALS_FILE $CLIENT_CONF_FILE
-if [ $? != 0 ]; then
-  echo "Failed to 'touch' configuration files ($?)"
+touch \$CLIENT_ID_FILE \$CREDENTIALS_FILE \$CLIENT_CONF_FILE
+if [ \$? != 0 ]; then
+  echo "Failed to 'touch' configuration files (\$?)"
   echo "Aborting installation..."
   date -Iseconds
   exit 1
 fi
 
-chmod u=rw,og-rwx $CLIENT_ID_FILE $CREDENTIALS_FILE $CLIENT_CONF_FILE
-touch $CLIENT_ID_FILE $CREDENTIALS_FILE
-if [ $? != 0 ]; then
-  echo "Failed to change permissions of configuration files ($?)"
+chmod u=rw,og-rwx \$CLIENT_ID_FILE \$CREDENTIALS_FILE \$CLIENT_CONF_FILE
+touch \$CLIENT_ID_FILE \$CREDENTIALS_FILE
+if [ \$? != 0 ]; then
+  echo "Failed to change permissions of configuration files (\$?)"
   echo "Aborting installation..."
   date -Iseconds
   exit 1
@@ -163,3 +170,7 @@ echo "Success - Baguette client successfully installed on system"
 date -Iseconds
 echo ""
 exit 0
+EOM
+
+# Run 'install-log.sh' script
+$BIN_DIRECTORY/install-log.sh $BASE_URL $APIKEY &> $LOGS_DIRECTORY/install.log
