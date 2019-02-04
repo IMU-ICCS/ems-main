e13d693c041221d0c1dc5fe04121210b0a415c07
areniewicz <areniewicz@7bulls.com>
2019-02-04 14:54:47 +0100

    logback configuration for ems

--- event-management/run.bat
+++ event-management/run.bat
@@ -26,9 +26,9 @@ rem Uncomment next line to run a Broker-CEP test scenario (BrokerCepServiceTest1
 rem set BROKER_CEP_TEST=-Drun-broker-cep-test=BrokerCepServiceTest5
 
 rem Use when Esper is packaged in control-service.jar
-rem java %JAVA_OPTS% -jar %BROKER_CEP_TEST% control-service\target\control-service.jar --logging.config=%MELODIC_CONFIG_DIR%\logback-spring.xml
+rem java %JAVA_OPTS% -jar %BROKER_CEP_TEST% control-service\target\control-service.jar --logging.config=file:${MELODIC_CONFIG_DIR}/logback-conf/logback-spring.xml
 rem Use when Esper is NOT packaged in control-service.jar
-java %JAVA_OPTS% -Djasypt.encryptor.password=%JASYPT_PASSWORD% -cp control-service\target\control-service.jar -Dloader.path=control-service\target\esper-7.1.0.jar %BROKER_CEP_TEST% org.springframework.boot.loader.PropertiesLauncher -nolog --logging.config=%MELODIC_CONFIG_DIR%\logback-spring.xml
+java %JAVA_OPTS% -Djasypt.encryptor.password=%JASYPT_PASSWORD% -cp control-service\target\control-service.jar -Dloader.path=control-service\target\esper-7.1.0.jar %BROKER_CEP_TEST% org.springframework.boot.loader.PropertiesLauncher -nolog --logging.config=file:${MELODIC_CONFIG_DIR}/logback-conf/logback-spring.xml
 rem e.g. --spring.config.location=%MELODIC_CONFIG_DIR%\
 rem e.g. --spring.config.name=application.properties
 
