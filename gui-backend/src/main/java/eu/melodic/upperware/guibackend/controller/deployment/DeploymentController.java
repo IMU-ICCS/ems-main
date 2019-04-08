ab5787284b5290647d8107ebfb31fe1bd7f6e0aa
areniewicz <areniewicz@7bulls.com>
2019-04-08 12:03:25 +0200

    upload xmi file, changes for new Mule api

--- gui-backend/src/main/java/eu/melodic/upperware/guibackend/controller/deployment/DeploymentController.java
+++ gui-backend/src/main/java/eu/melodic/upperware/guibackend/controller/deployment/DeploymentController.java
@@ -2,12 +2,14 @@ package eu.melodic.upperware.guibackend.controller.deployment;
 
 import eu.melodic.upperware.guibackend.controller.deployment.request.DeploymentRequest;
 import eu.melodic.upperware.guibackend.controller.deployment.response.DeploymentResponse;
+import eu.melodic.upperware.guibackend.controller.deployment.response.UploadXmiResponse;
 import eu.melodic.upperware.guibackend.service.deployment.DeploymentService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/deployment")
@@ -16,6 +18,20 @@ import org.springframework.web.bind.annotation.*;
 public class DeploymentController {
 
     private DeploymentService deploymentService;
+
+    @PostMapping(value = "/xmi")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UploadXmiResponse uploadXmi(@RequestParam("file") MultipartFile file) {
+        log.info("POST request for upload xmi file with name: {}", file.getResource().getFilename());
+        return deploymentService.uploadXmi(file);
+    }
+
+    @DeleteMapping(value = "/xmi/{xmiName}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteXmiModel(@PathVariable("xmiName") String xmiName) {
+        log.info("DELETE request for xmi model: {}", xmiName);
+        deploymentService.deleteXmiModel(xmiName);
+    }
 
     @PostMapping(value = "/process")
     @ResponseStatus(HttpStatus.CREATED)
---
+++ gui-backend/src/main/java/eu/melodic/upperware/guibackend/controller/deployment/response/UploadXmiResponse.java
@@ -0,0 +1,14 @@
+package eu.melodic.upperware.guibackend.controller.deployment.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UploadXmiResponse {
+    private String modelName;
+}
