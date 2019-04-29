b958abc59db57f69bcc4d226329f86716e7d560c
ipatini <ipatini@acci.gr>
2019-04-29 22:19:55 +0300

    EMS - Control Service - Added authorization check in EMS server (see also 'authorization-service' ). Renamed control service HTTPS keystore to ems-keystore.p12

---
+++ event-management/control-service/src/main/java/eu/melodic/event/control/webconf/WebMvcConfig.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2017 Institute of Communication and Computer Systems (imu.iccs.com)
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL
+ * was not distributed with this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ */
+
+package eu.melodic.event.control.webconf;
+
+import eu.melodic.security.authorization.client.AuthorizationServiceTomcatInterceptor;
+import eu.melodic.security.authorization.util.properties.AuthorizationServiceClientProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+@ComponentScan(basePackages={"eu.melodic.security.authorization.util.properties"})
+@Slf4j
+public class WebMvcConfig implements WebMvcConfigurer {
+    public final static String[] DEFAULT_PATHS_PROTECTED = { "/**" };
+    public final static String[] DEFAULT_PATHS_EXCLUDED = { };
+
+    @Autowired
+    private AuthorizationServiceClientProperties authProperties;
+
+    @Value("${authorization.enabled:true}")
+    private boolean authEnabled;
+    @Value("${authorization.paths-protected:}")
+    private String[] authPathsProtected;
+    @Value("${authorization.paths-excluded:}")
+    private String[] authPathsExcluded;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        if (!authEnabled || authProperties.getPdp().isDisabled()) {
+            log.warn("WebMvcConfig.addInterceptors(): Authorization check is disabled");
+        } else {
+            log.info("WebMvcConfig.addInterceptors(): Authorization check is enabled");
+
+            if (authPathsProtected==null || authPathsProtected.length==0) authPathsProtected = DEFAULT_PATHS_PROTECTED;
+            if (authPathsExcluded==null || authPathsExcluded.length==0) authPathsExcluded = DEFAULT_PATHS_EXCLUDED;
+            log.warn("WebMvcConfig.addInterceptors(): Authorization check: paths-protected={}, paths-excluded={}",
+                    authPathsProtected, authPathsExcluded);
+
+            log.debug("WebMvcConfig.addInterceptors(): Authorization properties: {}", authProperties);
+            registry
+                    .addInterceptor(AuthorizationServiceTomcatInterceptor.getSingleton(authProperties))
+                    .addPathPatterns(authPathsProtected)
+                    .excludePathPatterns(authPathsExcluded)
+            ;
+            log.debug("WebMvcConfig.addInterceptors(): Registered Authorization interceptor");
+        }
+    }
+}
