be726332c71068e0ffd787e85e329f6ea472f495
ipatini <ipatini@acci.gr>
2019-05-09 10:20:58 +0300

    .

--- event-management/broker-cep/src/main/java/eu/melodic/event/brokercep/BrokerCepService.java
+++ event-management/broker-cep/src/main/java/eu/melodic/event/brokercep/BrokerCepService.java
@@ -35,7 +35,6 @@ import java.nio.charset.Charset;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.*;
