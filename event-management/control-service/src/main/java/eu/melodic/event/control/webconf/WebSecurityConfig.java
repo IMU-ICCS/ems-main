8a78928ee90a0d22aa79f7774daf55b6fc50e2d6
ipatini <ipatini@acci.gr>
2019-05-31 13:19:29 +0300

    EMS - Control Service - Temporary workaround for JWT-authentication issue on /baguette/registerNode endpoint

--- event-management/control-service/src/main/java/eu/melodic/event/control/webconf/WebSecurityConfig.java
+++ event-management/control-service/src/main/java/eu/melodic/event/control/webconf/WebSecurityConfig.java
@@ -25,6 +25,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -137,6 +138,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             }
         }
 
+    }
+
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception {
+        webSecurity
+                .ignoring()
+                .antMatchers("/baguette/registerNode");
     }
 
     // Cors configuration
