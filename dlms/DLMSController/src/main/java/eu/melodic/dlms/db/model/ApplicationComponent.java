34df5c6a6f27088cd88b6478ce95766a5a04f899
dipesh <dipesh@simula.no>
2019-06-12 10:58:01 +0200

    event processing fixed

--- dlms/DLMSController/src/main/java/eu/melodic/dlms/db/model/ApplicationComponent.java
+++ dlms/DLMSController/src/main/java/eu/melodic/dlms/db/model/ApplicationComponent.java
@@ -1,11 +1,11 @@
 package eu.melodic.dlms.db.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,11 +13,11 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class ApplicationComponent {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
 	private String name;
 
