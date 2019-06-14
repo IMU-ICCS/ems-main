f13bbc9983056a2483b59cd83ebeff007a2235db
Marcin Prusiński <mprusinski@7bulls.com>
2019-06-14 14:47:25 +0200

    change of keystore importing of mule cert

--- event-management/bin/run.sh
+++ event-management/bin/run.sh
@@ -17,14 +17,14 @@ if [[ -z $PAASAGE_CONFIG_DIR ]]; then PAASAGE_CONFIG_DIR=$BASEDIR/config-files; 
 if [[ -z $JAR_PATH ]]; then JAR_PATH=$BASEDIR/control-service/target; export JAR_PATH; fi
 
 # Import MULE certificate
-MULE_CERT=$MELODIC_CONFIG_DIR/mule-server.crt
-if [[ -f ${MULE_CERT} ]]; then
-    echo "importing mule certificate"
-    keytool -noprompt -storepass changeit -import -alias mule -keystore /usr/lib/jvm/java-1.8-openjdk/jre/lib/security/cacerts -file ${MULE_CERT}
-    echo "importing mule certificate completed"
-else
-    echo "mule certificate not found: ${MULE_CERT}"
-fi
+#MULE_CERT=$MELODIC_CONFIG_DIR/mule-server.crt
+#if [[ -f ${MULE_CERT} ]]; then
+#    echo "importing mule certificate"
+#    keytool -noprompt -storepass changeit -import -alias mule -keystore /usr/lib/jvm/java-1.8-openjdk/jre/lib/security/cacerts -file ${MULE_CERT}
+#    echo "importing mule certificate completed"
+#else
+#    echo "mule certificate not found: ${MULE_CERT}"
+#fi
 
 # Initialize keystores and certificate
 # Uncomment next line to generate BrokerCEP keystore, truststore and certificate before EMS server launch
@@ -55,7 +55,7 @@ fi
 
 # Run EMS server
 # Uncomment next line to set JAVA runtime options
-# JAVA_OPTS=-Djavax.net.debug=all
+#JAVA_OPTS=-Djavax.net.debug=all
 export JAVA_OPTS
 
 echo "MELODIC_CONFIG_DIR=${MELODIC_CONFIG_DIR}"
@@ -64,7 +64,7 @@ echo "Starting EMS server..."
 # java $JAVA_OPTS -Djasypt.encryptor.password=$JASYPT_PASSWORD -Duser.timezone=Europe/Warsaw -Djava.security.egd=file:/dev/urandom -jar $JAR_PATH/control-service/target/control-service.jar --logging.config=file:$LOG_CONFIG_FILE
 
 # Use when Esper is NOT packaged in control-service.jar
-java $JAVA_OPTS -Djasypt.encryptor.password=$JASYPT_PASSWORD -Duser.timezone=Europe/Warsaw -Djava.security.egd=file:/dev/urandom -cp ${JAR_PATH}/control-service.jar -Dloader.path=${JAR_PATH}/esper-7.1.0.jar org.springframework.boot.loader.PropertiesLauncher --logging.config=file:$LOG_CONFIG_FILE 
+java $JAVA_OPTS -Djasypt.encryptor.password=$JASYPT_PASSWORD -Duser.timezone=Europe/Warsaw -Djava.security.egd=file:/dev/urandom -cp ${JAR_PATH}/control-service.jar -Dloader.path=${JAR_PATH}/esper-7.1.0.jar org.springframework.boot.loader.PropertiesLauncher --logging.config=file:$LOG_CONFIG_FILE
 
 # Extra parameters
 # e.g. --spring.config.location=$MELODIC_CONFIG_DIR
