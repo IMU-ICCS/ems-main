6bc3b7ecc22513e38f3a2d0724bb25a99dfa66cf
Sebastian.Schork <sebastian.schork@cas.de>
2019-07-25 11:10:08 +0200

    Reset admin password to "melodic"
    Change-Id: I409df9954a96a0751172801aa054fd0935e6ba78

--- mq-http-adapter/src/main/docker/run.sh
+++ mq-http-adapter/src/main/docker/run.sh
@@ -4,7 +4,15 @@ curl -XPOST 'http://ui-influxdb:8086/query' --data-urlencode 'q=CREATE DATABASE 
 
 sleep 30
 
-curl --user admin:admin -i -XPOST -H "Content-Type: application/json" "http://ui-grafana:3000/api/datasources" -d '
+curl --user admin:admin -XPUT -H "Content-Type: application/json" -d '{
+  "oldPassword": "admin",
+  "newPassword": "melodic",
+  "confirmNew": "melodic"
+}' http://ui-grafana:3000/api/user/password
+
+sleep 5
+
+curl --user admin:melodic -i -XPOST -H "Content-Type: application/json" "http://ui-grafana:3000/api/datasources" -d '
 {
         "name": "InfluxDB",
         "type": "influxdb",
