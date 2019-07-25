6aa9411393f37c4f7eafec1ee599a6ec89134ae1
Sebastian.Schork <sebastian.schork@cas.de>
2019-07-25 10:16:46 +0200

    Create Grafana data source on startup
    Change-Id: I9f93197752a7a44b83a00a6ff2b6e2f8b97cb08f

--- mq-http-adapter/src/main/docker/run.sh
+++ mq-http-adapter/src/main/docker/run.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
 curl -XPOST 'http://ui-influxdb:8086/query' --data-urlencode 'q=CREATE DATABASE melodic_ui'
+
+curl --user admin:admin -i -XPOST -H "Content-Type: application/json" "http://ui-grafana:3000/api/datasources" -d '
+{
+        "name": "InfluxDB",
+        "type": "influxdb",
+        "url": "http://ui-influxdb:8086",
+        "database": "melodic_ui",
+        "isDefault": true,
+        "access": "proxy"
+}'
+
+
 java -jar mq-http-adapter.jar
