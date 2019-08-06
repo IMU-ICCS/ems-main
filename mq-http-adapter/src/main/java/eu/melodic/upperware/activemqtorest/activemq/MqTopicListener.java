b485aa0ca8cfc700dd700122309c7bb9f77dec76
Sebastian.Schork <sebastian.schork@cas.de>
2019-08-06 12:32:36 +0200

    Remove unused logger imports
    Change-Id: I60891b7f0a416e1fd6da003cc2ffe188d764d23c

--- mq-http-adapter/src/main/java/eu/melodic/upperware/activemqtorest/activemq/MqTopicListener.java
+++ mq-http-adapter/src/main/java/eu/melodic/upperware/activemqtorest/activemq/MqTopicListener.java
@@ -9,8 +9,6 @@ import javax.jms.JMSException;
 
 import org.apache.activemq.command.ActiveMQMessage;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
