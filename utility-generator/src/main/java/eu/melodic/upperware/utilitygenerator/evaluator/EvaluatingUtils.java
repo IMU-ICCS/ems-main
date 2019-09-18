7748de79fc6211e81f923f5bf10637cdc72f4bb2
Marta Różańska <mrozanska@7bulls.com>
2019-09-18 14:01:27 +0200

    support new types of variables: Latitude and Longitude

--- utility-generator/src/main/java/eu/melodic/upperware/utilitygenerator/evaluator/EvaluatingUtils.java
+++ utility-generator/src/main/java/eu/melodic/upperware/utilitygenerator/evaluator/EvaluatingUtils.java
@@ -117,13 +117,19 @@ public class EvaluatingUtils {
                 case STORAGE:
                     predicates.add(getStoragePredicate((int) var.getValue()));
                     break;
+                case LATITUDE:
+                    log.debug("Creating getLatitudePredicate for value {}", var.getValue());
+                    predicates.add(getLatitudePredicate((double) var.getValue()));
+                    break;
+                case LONGITUDE:
+                    log.debug("Creating getLongitudePredicate for value {}", var.getValue());
+                    predicates.add(getLongitudePredicate((double) var.getValue()));
+                    break;
                 case LOCATION:
                 case CARDINALITY:
                 case PROVIDER:
                 case CPU:
-                case LATITUDE:
-                case LONGITUDE:
-                    break;
+                break;
             }
         }
 
