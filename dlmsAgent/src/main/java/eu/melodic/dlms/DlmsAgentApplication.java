03f23470f71a8fa263fdb975b4659766c5813c1c
dipesh <dipesh@simula.no>
2019-09-23 11:45:34 +0200

    comments addressed

--- dlmsAgent/src/main/java/eu/melodic/dlms/DlmsAgentApplication.java
+++ dlmsAgent/src/main/java/eu/melodic/dlms/DlmsAgentApplication.java
@@ -48,7 +48,8 @@ public class DlmsAgentApplication {
 	}
 
 	/**
-	 * CommandLineRunner to start a timer directly after startup, which then collects metrics and publishes them via JMS.
+	 * CommandLineRunner to start a timer directly after startup, which then
+	 * collects metrics and publishes them via JMS.
 	 */
 	@Bean
 	public CommandLineRunner run() {
@@ -59,48 +60,49 @@ public class DlmsAgentApplication {
 
 			String url = System.getProperties().getProperty("mode");
 			log.info("Application started and URL {} identified", url);
-			
-			String publicIp= System.getProperties().getProperty("ip.public");
-			String webServiceUrl = System.getProperties().getProperty("webServiceUrl")+publicIp;
+
+			String publicIp = System.getProperties().getProperty("ip.public");
+			String webServiceUrl = System.getProperties().getProperty("webServiceUrl") + publicIp;
 
 			SendToDlmsAgent sendToDlmsAgent = getSendToDlmsAgent(webServiceUrl);
-			String appComp = "";
-			
+
 			// to stop program from going to a loop
 			// this needs to be modified to get information from melodic when it is ready
 			int counter = 1;
-			
-			while (sendToDlmsAgent == null && isBothNull(sendToDlmsAgent)) {
+			boolean isNull = isNull(sendToDlmsAgent);
+			while (isNull) { // repeat it until it is not null
 				// sleep needs to be changed later on
 				Thread.sleep(20000);
-				counter ++;
-				
-				if (counter>10) {
+				counter++;
+
+				if (counter > 10) {
 					log.error("Did not find the component id. Exiting now ...");
 					break;
 				}
 				log.debug("Did not find the component id. Waiting ....");
+				isNull = isNull(sendToDlmsAgent);
 			}
-			
+			String appComp = runCommands(sendToDlmsAgent);
+
 			TimerTask timerTask = new TimerTask() {
 				@Override
 				public void run() {
 					log.info("Running metrics collection for {}", url);
 
-					switch(metricsRange) {
-						case ALLUXIO: {
-							metricsController.collectAlluxioMetrics(url);
-							break;
-						}
-						case MY_SQL: {
-							metricsController.collectMySqlMetrics();
-							break;
-						}
-						case ALL: {
-							metricsController.collectAlluxioMetrics(url);
-							metricsController.collectMySqlMetrics();
-							break;
-						}
+					switch (metricsRange) {
+					case ALLUXIO: {
+						metricsController.collectAlluxioMetrics(url);
+						break;
+					}
+					case MY_SQL: {
+						metricsController.collectMySqlMetrics();
+						break;
+					}
+					case ALL: {
+						metricsController.collectAlluxioMetrics(url);
+						metricsController.collectMySqlMetrics();
+						break;
+					}
 					}
 					metricsController.sendMetrics(appComp);
 				}
@@ -108,52 +110,55 @@ public class DlmsAgentApplication {
 
 			Timer timer = new Timer();
 			timer.schedule(timerTask, DELAY_AFTER_STARTUP, CALL_INTERVAL);
-			log.info("Started timer with delay=" + DELAY_AFTER_STARTUP / 1000 + " sec. and interval=" + CALL_INTERVAL / 1000 + " sec.");
+			log.info("Started timer with delay=" + DELAY_AFTER_STARTUP / 1000 + " sec. and interval="
+					+ CALL_INTERVAL / 1000 + " sec.");
 		};
 	}
-	
+
 	/**
 	 * Get contents form the web service url
 	 */
-	public SendToDlmsAgent getSendToDlmsAgent(String webServiceUrl) {		
+	public SendToDlmsAgent getSendToDlmsAgent(String webServiceUrl) {
 		try {
 			RestTemplate restTemplate = new RestTemplate();
 			URI uri = new URI(webServiceUrl);
 			HttpHeaders headers = new HttpHeaders();
 			headers.setContentType(MediaType.APPLICATION_JSON);
-			
+
 			HttpEntity<SendToDlmsAgent> entity = new HttpEntity<>(headers);
 			ResponseEntity<SendToDlmsAgent> response = restTemplate.exchange(uri, HttpMethod.GET, entity,
 					SendToDlmsAgent.class);
-			return response.getBody();		
+			return response.getBody();
 		} catch (URISyntaxException e) {
 			log.error("Problem getting the contents from dlms web service url");
 		}
-		return null;	
+		return null;
 	}
-	
+
 	/**
-	 * Execute command and send back component id
+	 * Check that both command to mount and compnent id is present
 	 */
-	public boolean isBothNull(SendToDlmsAgent sendToDlmsAgent) {
-		if (sendToDlmsAgent!=null) {
-			return sendToDlmsAgent.getCommand()!=null && sendToDlmsAgent.getComponentId()!=null;
+	public boolean isNull(SendToDlmsAgent sendToDlmsAgent) {
+		if (sendToDlmsAgent != null) { // safety check first
+			return sendToDlmsAgent.getCommand() == null && sendToDlmsAgent.getComponentId() == null;
 		}
 		return true;
 	}
-	
+
+	/**
+	 * Execute command and send back component id
+	 */
 	public String runCommands(SendToDlmsAgent sendToDlmsAgent) {
 		String cmd = sendToDlmsAgent.getCommand();
 		Process p;
-		
+
 		try {
 			p = Runtime.getRuntime().exec(cmd);
 			p.waitFor();
 		} catch (Exception e) {
 			log.error("There was a problem while executing the command from dlms web service api");
-		} 
+		}
 		return sendToDlmsAgent.getComponentId();
 	}
-	
-	
+
 }
