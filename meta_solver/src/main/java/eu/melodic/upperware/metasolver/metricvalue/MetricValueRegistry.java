470074656479fe37fc8d57acbafb0a51d06a3f7e
ipatini <ipatini@acci.gr>
2019-10-16 10:59:42 +0300

    MetaSolver - Updated copyright notice

--- meta_solver/src/main/java/eu/melodic/upperware/metasolver/metricvalue/MetricValueRegistry.java
+++ meta_solver/src/main/java/eu/melodic/upperware/metasolver/metricvalue/MetricValueRegistry.java
@@ -1,10 +1,9 @@
 /*
- * Copyright (C) 2017 Institute of Communication and Computer Systems (imu.iccs.com)
+ * Copyright (C) 2017-2019 Institute of Communication and Computer Systems (imu.iccs.gr)
  *
- * This Source Code Form is subject to the terms of the
- * Mozilla Public License, v. 2.0. If a copy of the MPL
- * was not distributed with this file, You can obtain one at
- * http://mozilla.org/MPL/2.0/.
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at
+ * https://www.mozilla.org/en-US/MPL/2.0/
  */
 
 package eu.melodic.upperware.metasolver.metricvalue;
