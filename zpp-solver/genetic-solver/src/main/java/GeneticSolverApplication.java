88c97dca4b431773a8d5c783f492153a02c7c5a1
Mikchael <pazdziorekmichal@gmail.com>
2020-01-16 20:17:53 +0100

    Add spring boot frame

---
+++ zpp-solver/genetic-solver/src/main/java/GeneticSolverApplication.java
@@ -0,0 +1,21 @@
+import eu.melodic.cache.properties.CacheProperties;
+import eu.melodic.upperware.penaltycalculator.PenaltyFunctionProperties;
+import eu.melodic.upperware.utilitygenerator.properties.UtilityGeneratorProperties;
+import eu.paasage.upperware.security.authapi.properties.MelodicSecurityProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+
+@EnableAsync
+@Configuration
+@SpringBootApplication
+@EnableConfigurationProperties({UtilityGeneratorProperties.class, MelodicSecurityProperties.class, CacheProperties.class, PenaltyFunctionProperties.class})
+public class GeneticSolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneticSolverApplication.class, args);
+    }
+}
