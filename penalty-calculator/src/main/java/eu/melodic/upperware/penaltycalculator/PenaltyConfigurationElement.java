35b7cd2dad60e67caf3a4a64cbdd05988b46e5ae
ipatini <ipatini@acci.gr>
2020-01-16 16:34:57 +0200

    Penalty Calculator - Updated copyright notice. Removed JAR

--- penalty-calculator/src/main/java/eu/melodic/upperware/penaltycalculator/PenaltyConfigurationElement.java
+++ penalty-calculator/src/main/java/eu/melodic/upperware/penaltycalculator/PenaltyConfigurationElement.java
@@ -1,4 +1,5 @@
-/* * Copyright (C) 2017 7bulls.com
+/*
+ * Copyright (C) 2017 7bulls.com
  *
  * This Source Code Form is subject to the terms of the
  * Mozilla Public License, v. 2.0. If a copy of the MPL
