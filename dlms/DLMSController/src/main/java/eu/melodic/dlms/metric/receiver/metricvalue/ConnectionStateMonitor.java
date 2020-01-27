b16a775930d4e9222f4f5f6274e1596019e03250
LSZ <lszymanski@7bulls.com>
2020-01-27 16:22:10 +0100

    All changes with regard to MEL-976 task - adding network latency metric support

---
+++ dlms/DLMSController/src/main/java/eu/melodic/dlms/metric/receiver/metricvalue/ConnectionStateMonitor.java
@@ -0,0 +1,33 @@
+package eu.melodic.dlms.metric.receiver.metricvalue;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.activemq.transport.TransportListener;
+
+import java.io.IOException;
+
+@Slf4j
+class ConnectionStateMonitor implements TransportListener {
+    @Override
+    public void onCommand(Object command)
+    {
+        log.debug("Command detected: '" + command + "'");
+    }
+
+    @Override
+    public void onException(IOException exception)
+    {
+        log.error("Exception detected: '" + exception + "'");
+    }
+
+    @Override
+    public void transportInterupted()
+    {
+        log.error("Transport interuption detected.");
+    }
+
+    @Override
+    public void transportResumed()
+    {
+        log.info("Transport resumption detected.");
+    }
+}
