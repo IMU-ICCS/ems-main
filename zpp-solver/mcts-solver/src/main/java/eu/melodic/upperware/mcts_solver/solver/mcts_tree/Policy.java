f609abf9028f8f955bed5a113be94925b190c7a9
Michal Jankowski <mj370955@students.mimuw.edu.pl>
2020-03-02 19:06:08 +0100

    Changed from SolutionImpl to Solution in Policy.

--- zpp-solver/mcts-solver/src/main/java/eu/melodic/upperware/mcts_solver/solver/mcts_tree/Policy.java
+++ zpp-solver/mcts-solver/src/main/java/eu/melodic/upperware/mcts_solver/solver/mcts_tree/Policy.java
@@ -1,7 +1,5 @@
 package eu.melodic.upperware.mcts_solver.solver.mcts_tree;
 
-import eu.melodic.upperware.mcts_solver.solver.mcts_tree_impl.SolutionImpl;
-
 public interface Policy {
 
     Solution finishPath(Path path);
--- zpp-solver/mcts-solver/src/main/java/eu/melodic/upperware/mcts_solver/solver/mcts_tree_impl/RandomPolicyImpl.java
+++ zpp-solver/mcts-solver/src/main/java/eu/melodic/upperware/mcts_solver/solver/mcts_tree_impl/RandomPolicyImpl.java
@@ -2,10 +2,11 @@ package eu.melodic.upperware.mcts_solver.solver.mcts_tree_impl;
 
 import eu.melodic.upperware.mcts_solver.solver.mcts_tree.Path;
 import eu.melodic.upperware.mcts_solver.solver.mcts_tree.Policy;
+import eu.melodic.upperware.mcts_solver.solver.mcts_tree.Solution;
 
 public class RandomPolicyImpl implements Policy {
     @Override
-    public SolutionImpl finishPath(Path path) {
+    public Solution finishPath(Path path) {
         //TODO
         return null;
     }
