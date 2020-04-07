bc59a3a7657fe7945d003d545534520d113687ff
tc360950 <tc360950@students.mimuw.edu.pl>
2020-04-07 18:56:04 +0200

    removed duplicated code form NCSolverCoordinator:

--- zpp-solver/node-candidates-solver/src/main/java/eu/melodic/upperware/nc_solver/NCSolverCoordinator.java
+++ zpp-solver/node-candidates-solver/src/main/java/eu/melodic/upperware/nc_solver/NCSolverCoordinator.java
@@ -146,7 +146,7 @@ public class NCSolverCoordinator {
 
         List<CpVariableValue> values = cp.getCpVariables()
                 .stream()
-                .map(var -> createCpVariableValue(bestSolution, var))
+                .map(var -> CpVariableCreator.createCpVariableValue(bestSolution, var))
                 .collect(Collectors.toList());
 
         log.info("Solution with best utility {}:", maxUtility);
@@ -155,53 +155,6 @@ public class NCSolverCoordinator {
         }
 
         cp.getSolution().add(createSolution(maxUtility, values));
-    }
-
-    private CpVariableValue createCpVariableValue(List<VariableValueDTO> bestSolution, CpVariable var) {
-        log.debug("Considering variable: {}", var.getId());
-        Domain dom = var.getDomain();
-        if (dom instanceof RangeDomain) {
-            RangeDomain rd = (RangeDomain) dom;
-            NumericValueUpperware from = rd.getFrom();
-            if (from instanceof IntegerValueUpperware) {
-                Number variableValue = findVariableValue(bestSolution, var);
-                return CPModelTool.createCpVariableValue(var, createIntegerValueUpperware(variableValue.intValue()));
-            } else if (from instanceof LongValueUpperware) {
-                Number variableValue = findVariableValue(bestSolution, var);
-                return CPModelTool.createCpVariableValue(var, createLongValueUpperware(variableValue.longValue()));
-            } else if (from instanceof DoubleValueUpperware) {
-                Number variableValue = findVariableValue(bestSolution, var);
-                return CPModelTool.createCpVariableValue(var, createDoubleValueUpperware(variableValue.doubleValue()));
-            } else {
-                Number variableValue = findVariableValue(bestSolution, var);
-                return CPModelTool.createCpVariableValue(var, createFloatValueUpperware(variableValue.floatValue()));
-            }
-        } else if (dom instanceof NumericDomain) {
-            NumericDomain nd = (NumericDomain) dom;
-            BasicTypeEnum type = nd.getType();
-            if (type.equals(BasicTypeEnum.INTEGER)) {
-                Number variableValue = findVariableValue(bestSolution, var);
-                return CPModelTool.createCpVariableValue(var, createIntegerValueUpperware(variableValue.intValue()));
-            } else if (type.equals(BasicTypeEnum.LONG)) {
-                Number variableValue = findVariableValue(bestSolution, var);
-                return CPModelTool.createCpVariableValue(var, createLongValueUpperware(variableValue.longValue()));
-            } else if (type.equals(BasicTypeEnum.DOUBLE)) {
-                Number variableValue = findVariableValue(bestSolution, var);
-                return CPModelTool.createCpVariableValue(var, createDoubleValueUpperware(variableValue.doubleValue()));
-            } else {
-                Number variableValue = findVariableValue(bestSolution, var);
-                return CPModelTool.createCpVariableValue(var, createFloatValueUpperware(variableValue.floatValue()));
-            }
-        }
-        throw new RuntimeException("Unsupported method type: " + dom.getClass());
-    }
-
-    private Number findVariableValue(List<VariableValueDTO> bestSolution, CpVariable var) {
-        return bestSolution
-                .stream()
-                .filter(variableValueDTO -> variableValueDTO.getName().equals(var.getId()))
-                .findFirst().orElseThrow(() -> new RuntimeException("Could not find VariableValue for " + var.getId()))
-                .getValue();
     }
 
     private ConstraintProblem getCPFromFile(String pathName) {
