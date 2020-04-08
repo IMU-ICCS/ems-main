e2824b1c47be635f0a6209c0316c1c7988a8ede7
tc360950 <tc360950@students.mimuw.edu.pl>
2020-04-08 12:00:01 +0200

    added comment to Policy

--- zpp-solver/mcts-solver/src/main/java/eu/melodic/upperware/mcts_solver/solver/mcts/tree/Policy.java
+++ zpp-solver/mcts-solver/src/main/java/eu/melodic/upperware/mcts_solver/solver/mcts/tree/Policy.java
@@ -2,5 +2,9 @@ package eu.melodic.upperware.mcts_solver.solver.mcts.tree;
 
 public interface Policy {
     Solution finishPath(Path path);
+    /*
+    If the policy is not able to finish path below @minDepthSubtreeRemoval - i.e. empty solution is returned
+    - then it means that no valid solution exists in the subtree
+     */
     int minDepthSubtreeRemoval();
 }
