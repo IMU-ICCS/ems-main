f6b8bacc9e0e71bfdab0e3c9caa86fb0ce96f156
Jan Marchel <jmarchel@7bulls.com>
2021-04-13 11:38:38 +0200

    deleting dlmsutility from CamelMetadata

--- melodic-commons/src/main/java/eu/passage/upperware/commons/model/tools/metadata/CamelMetadata.java
+++ melodic-commons/src/main/java/eu/passage/upperware/commons/model/tools/metadata/CamelMetadata.java
@@ -11,30 +11,29 @@ import java.util.List;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum CamelMetadata {
 
-    CORES("CPU", VariableType.CORES, false, false, false),
-    RAM("RAM", VariableType.RAM, false, false, false),
-    STORAGE("Storage", VariableType.STORAGE, false, false, false),
-    CARDINALITY("Cardinality", VariableType.CARDINALITY, false, false, false),
-    LATITUDE("latitude", VariableType.LATITUDE, false, false, false),
-    LONGITUDE("longitude", VariableType.LONGITUDE, false, false, false),
+    CORES("CPU", VariableType.CORES, false, false),
+    RAM("RAM", VariableType.RAM, false,false),
+    STORAGE("Storage", VariableType.STORAGE, false, false),
+    CARDINALITY("Cardinality", VariableType.CARDINALITY, false, false),
+    LATITUDE("latitude", VariableType.LATITUDE, false, false),
+    LONGITUDE("longitude", VariableType.LONGITUDE, false, false),
 
-    PROVIDER("", VariableType.PROVIDER, false, false, false),
+    PROVIDER("", VariableType.PROVIDER, false, false),
 
-    PRICE("Cost", null, true, false, false),
+    PRICE("Cost", null, true, false),
 
-    UNMOVEABLE("Unmoveable", null, false, false, false),
+    UNMOVEABLE("Unmoveable", null, false, false),
 
-    AFFINITY_AWARENESS("AffinityAwareness", null, false, true, false),
-    DATA_CENTRE_AWARENESS("DataCentreAwareness", null, false, true, false),
-    SOURCE_AWARENESS("SourceAwareness", null,false, true, false),
+    AFFINITY_AWARENESS("AffinityAwareness", null, false, false),
+    DATA_CENTRE_AWARENESS("DataCentreAwareness", null, false, false),
+    SOURCE_AWARENESS("SourceAwareness", null,false, false),
 
-    PENALTY("AdapterUtility", null, false, false, true),
-    RECONFIGURATION_TIME("ReconfigurationTime", null, false, false, true);
+    PENALTY("AdapterUtility", null, false, true),
+    RECONFIGURATION_TIME("ReconfigurationTime", null, false, true);
 
     public String camelName;
     public VariableType variableType;
     public boolean onNodeCandidate;
-    public boolean dlmsUtility;
     public boolean reconfigurationPenalty;
 
 
