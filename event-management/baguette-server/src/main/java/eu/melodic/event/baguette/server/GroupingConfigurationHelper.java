d5e5004a15d1e6830036b7a51e23ff493cbaa12d
ipatini <ipatini@acci.gr>
2021-04-17 19:38:06 +0300

    EMS: Baguette Server: Renamed 'GroupingConfiguration' class to 'GroupingConfigurationHelper' and turned it into a GroupingConfiguration factory with 'newGroupingConfiguration' static method. Method 'getConfigurationMap()' has been removed.

---
+++ event-management/baguette-server/src/main/java/eu/melodic/event/baguette/server/GroupingConfigurationHelper.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2017-2019 Institute of Communication and Computer Systems (imu.iccs.gr)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v2.0, unless
+ * Esper library is used, in which case it is subject to the terms of General Public License v2.0.
+ * If a copy of the MPL was not distributed with this file, you can obtain one at
+ * https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+package eu.melodic.event.baguette.server;
+
+import eu.melodic.event.util.GroupingConfiguration;
+
+import java.util.Properties;
+
+/**
+ * Baguette Client Configuration creation helper
+ */
+public class GroupingConfigurationHelper {
+    public static GroupingConfiguration newGroupingConfiguration(String groupingName, Properties config, BaguetteServer server) {
+        return GroupingConfiguration.builder()
+                .name( groupingName )
+                .config( config )
+                .eventTypeNames( server.getTopicsForGrouping(groupingName) )
+                .rules( server.getRulesForGrouping(groupingName) )
+                .connections( server.getTopicConnectionsForGrouping(groupingName) )
+                .constants( server.getConstants() )
+                .functionDefinitions( server.getFunctionDefinitions() )
+                .brokerUsername( server.getBrokerUsername() )
+                .brokerPassword( server.getBrokerPassword() )
+                .build();
+    }
+}
