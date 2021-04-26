d2d160cc6d11dd68cc47820201267357b94b4a00
Jan Marchel <jmarchel@7bulls.com>
2021-04-26 18:04:20 +0200

    adding restart methods

--- gui-backend/src/main/java/eu/melodic/upperware/guibackend/service/environment/EnvironmentService.java
+++ gui-backend/src/main/java/eu/melodic/upperware/guibackend/service/environment/EnvironmentService.java
@@ -1,19 +1,41 @@
 package eu.melodic.upperware.guibackend.service.environment;
 
 import eu.melodic.upperware.guibackend.properties.GuiBackendProperties;
+import eu.melodic.upperware.guibackend.service.deployment.DeploymentService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.List;
+
 @Service
 @Slf4j
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class EnvironmentService {
-
     private GuiBackendProperties guiBackendProperties;
+    private DeploymentService deploymentService;
 
     // todo
     public void checkComponentsStatus() {
+    }
+
+    public void restartMelodic() throws IOException {
+        List<String> allXmiModels = deploymentService.getAllXmiModels();
+        for(String xmiModel : allXmiModels)
+        deploymentService.deleteXmiModel(xmiModel);
+        runRestartScript();
+    }
+
+    public void runRestartScript() throws IOException {
+        boolean isWindows = System.getProperty("os.name")
+                .toLowerCase().startsWith("windows");
+
+        if (isWindows) {
+            Runtime.getRuntime().exec("cmd.exe");
+        } else {
+            Runtime.getRuntime().exec("./restartscript.sh");
+        }
     }
 }
