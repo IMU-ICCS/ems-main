dec7c5e119f6576a1d8b777053dab610d6dd3a24
ipatini <ipatini@acci.gr>
2021-05-05 15:54:02 +0300

    EMS: Broker-CEP: Minor improvement: Added 'newEvent(double,int,String...)' method in CepEvalFunction class

--- event-management/broker-cep/src/main/java/eu/melodic/event/brokercep/cep/CepEvalFunction.java
+++ event-management/broker-cep/src/main/java/eu/melodic/event/brokercep/cep/CepEvalFunction.java
@@ -82,13 +82,17 @@ public class CepEvalFunction {
     }
 
     public static EventMap/*MetricEvent*/ newEvent(double metricValue, String... params) {
+        return newEvent(metricValue, 1);
+    }
+
+    public static EventMap/*MetricEvent*/ newEvent(double metricValue, int level, String... params) {
         log.debug(">> ---------------------------------------------------------------------------");
         log.debug(">> newEvent:   metric-value:  {}", metricValue);
         log.debug(">> newEvent:   params-length: {}", params.length);
 
         // Add metric value
-        EventMap event = new EventMap(metricValue, 1, System.currentTimeMillis());
-        //MetricEvent event = new MetricEvent(metricValue, 1, System.currentTimeMillis());
+        EventMap event = new EventMap(metricValue, level, System.currentTimeMillis());
+        //MetricEvent event = new MetricEvent(metricValue, level, System.currentTimeMillis());
 
         // Add extra parameters
         for (int i = 0; i < params.length; i += 2) {
