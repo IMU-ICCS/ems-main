38ea34153271f4403d27eaf339894cdcbc52b135
ipatini <ipatini@acci.gr>
2021-05-06 21:28:26 +0300

    EMS: Updated license notice to (2017-2022)

--- event-management/broker-client/client.bat
+++ event-management/broker-client/client.bat
@@ -1,6 +1,6 @@
 @echo off
 ::
-:: Copyright (C) 2017-2019 Institute of Communication and Computer Systems (imu.iccs.gr)
+:: Copyright (C) 2017-2022 Institute of Communication and Computer Systems (imu.iccs.gr)
 ::
 :: This Source Code Form is subject to the terms of the Mozilla Public License, v2.0, unless 
 :: Esper library is used, in which case it is subject to the terms of General Public License v2.0.
