99eb811d34f163f44181e6c506b73376d9f561d3
LSZ <lszymanski@7bulls.com>
2021-06-08 12:43:31 +0200

    to fix the issue with submitting the job that already has been submitted the PlanType enum has been added to indicate what type of plan we are dealing with. Now only for PlanType.CONFIGURATION the job is submitted to ProActive.

--- adapter/src/main/java/eu/melodic/upperware/adapter/plangenerator/DefaultPlanGenerator.java
+++ adapter/src/main/java/eu/melodic/upperware/adapter/plangenerator/DefaultPlanGenerator.java
@@ -37,7 +37,7 @@ public class DefaultPlanGenerator implements PlanGenerator {
     log.info("Building configuration plan");
     ComparableModel compModel = converter.toComparableModel(newModel);
     SimpleDirectedGraph<Task, DefaultEdge> graph = generator.generateGraph(compModel);
-    Plan plan = new SimplePlan(format("%s configuration plan", newModel.getName()), graph);
+    Plan plan = new SimplePlan(format("%s configuration plan", newModel.getName()), graph, PlanType.CONFIGURATION);
     log.info("Built plan: {}", plan);
     return plan;
   }
@@ -61,7 +61,7 @@ public class DefaultPlanGenerator implements PlanGenerator {
     log.info("New comparableModel: {}", newCompModel);
 
     SimpleDirectedGraph<Task, DefaultEdge> graph = generator.generateGraph(newCompModel, oldCompModel);
-    Plan plan = new SimplePlan(format("%s->%s reconfiguration plan", oldModel.getName(), newModel.getName()), graph);
+    Plan plan = new SimplePlan(format("%s->%s reconfiguration plan", oldModel.getName(), newModel.getName()), graph, PlanType.RECONFIGURATION);
     log.info("Built plan: {}", plan);
     return plan;
   }
