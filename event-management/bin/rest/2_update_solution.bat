0b2cf39e4e3e6ab6ebc3664a7bddfef5a3c3fbd4
ipatini <ipatini@acci.gr>
2021-07-23 17:00:55 +0300

    EMS: Improved test scripts to accept EMS API Key and JWT Token as parameters (usage: <script> [("s"|-) [(<api-key>|-) [(<jwt-token>|-)]]]

--- event-management/bin/rest/2_update_solution.bat
+++ event-management/bin/rest/2_update_solution.bat
@@ -1,2 +1,7 @@
 @echo off
-curl -k --request POST "http%1://localhost:8092/updateSolution" --header "Content-Type: application/json" --data "@bin\rest\2_update_solution_payload.txt"
+setlocal
+set s=
+if not "%1x"=="x" if "%1x"=="sx" set s=s
+if not "%2x"=="x" if not "%2x"=="-x" set token=--header "Authorization: Bearer %2"
+curl -v -k --request POST "http%s%://localhost:8092/updateSolution" %token% --header "Content-Type: application/json" --data "@bin\rest\2_update_solution_payload.txt"
+endlocal
