69d474e2622b9bf28ed73ecff457afd2b270dcae
ipatini <ipatini@acci.gr>
2021-07-28 21:20:45 +0300

    EMS: Control Service: Added EMS information collection feature and plugged it into Actuator endpoints and JMX. Moved build info printing from ControlServiceApplication to ControlServiceBuildInfoEndpoint class.

---
+++ event-management/control-service/src/main/java/eu/melodic/event/control/info/ControlServiceInfoEndpointExtension.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2017-2022 Institute of Communication and Computer Systems (imu.iccs.gr)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v2.0, unless
+ * Esper library is used, in which case it is subject to the terms of General Public License v2.0.
+ * If a copy of the MPL was not distributed with this file, you can obtain one at
+ * https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+package eu.melodic.event.control.info;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
+import org.springframework.boot.actuate.endpoint.web.annotation.EndpointWebExtension;
+import org.springframework.boot.actuate.info.InfoEndpoint;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@EndpointWebExtension(endpoint = InfoEndpoint.class)
+public class ControlServiceInfoEndpointExtension {
+
+    private final ApplicationContext applicationContext;
+    private final InfoEndpoint delegate;
+
+    @ReadOperation
+    public WebEndpointResponse<Map> info() {
+        Map<String, Object> info = new HashMap<>(this.delegate.info());
+        info.put("ems-build-info", applicationContext.getBean(ControlServiceBuildInfoEndpoint.class).infoMap());
+        info.put("ems-live-info", "ToDo");
+        return new WebEndpointResponse<>(info, 200);
+    }
+}
