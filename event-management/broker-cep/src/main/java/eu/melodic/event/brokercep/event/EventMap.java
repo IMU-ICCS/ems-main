232d7723c46543af58d05cdbaf2d728a701bd7b9
ipatini <ipatini@acci.gr>
2021-08-02 17:49:37 +0300

    EMS: Broker-CEP: Modified EventMap to process 'key' and 'value' before putting (i.e. strip quotes, convert to value to the expected type). Improved EventMap standard fields definition.

--- event-management/broker-cep/src/main/java/eu/melodic/event/brokercep/event/EventMap.java
+++ event-management/broker-cep/src/main/java/eu/melodic/event/brokercep/event/EventMap.java
@@ -11,19 +11,60 @@ package eu.melodic.event.brokercep.event;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Data
 @Slf4j
 @EqualsAndHashCode(callSuper = false)
-public class EventMap extends HashMap<String, Object> implements Serializable {
-    public final static String[] PROPERTY_NAMES_ARRAY = new String[]{"metricValue", "level", "timestamp"};
-    public final static Class[] PROPERTY_CLASSES_ARRAY = new Class[]{Double.class, Integer.class, Long.class};
+public class EventMap extends LinkedHashMap<String, Object> implements Serializable {
 
+    // Standard Event fields configuration
+    @Data
+    @RequiredArgsConstructor
+    public static class EventField {
+        private final String name;
+        private final Class<?> type;
+        private final boolean nullable;
+        private final boolean skillIfNull;
+        private final Function<EventMap,Object> defaultValue;
+    }
+
+    public final static List<EventField> STANDARD_EVENT_FIELDS = Collections.unmodifiableList(Arrays.asList(
+            new EventField("metricValue", Double.class, false, false, null),
+            new EventField("level", Integer.class, true, true, null),
+            new EventField("timestamp", Long.class, true, true, (f)->System.currentTimeMillis())
+    ));
+
+    public final static Map<String,EventField> STANDARD_EVENT_FIELDS_MAP = STANDARD_EVENT_FIELDS.stream()
+            .collect(Collectors.toMap(EventField::getName, x->x));
+
+    public final static String[] PROPERTY_NAMES_ARRAY = STANDARD_EVENT_FIELDS.stream()
+            .map(EventField::getName)
+            .collect(Collectors.toList())
+            .toArray(new String[0]);
+    public final static Class<?>[] PROPERTY_CLASSES_ARRAY = STANDARD_EVENT_FIELDS.stream()
+            .map(EventField::getType)
+            .collect(Collectors.toList())
+            .toArray(new Class[0]);
+
+    public static String[] getPropertyNames() {
+        return PROPERTY_NAMES_ARRAY;
+    }
+
+    public static Class<?>[] getPropertyClasses() {
+        return PROPERTY_CLASSES_ARRAY;
+    }
+
+
+    // Constructors
     public EventMap() {
         super();
     }
@@ -48,32 +89,8 @@ public class EventMap extends HashMap<String, Object> implements Serializable {
         put("timestamp", timestamp);
     }
 
-    @Override
-    public Object put(String key, Object value) {
-        log.info("Event.put: BEGIN: key={}, value={}, value-class={}", key, value, value.getClass().getName());
-        if ("metricValue".equals(key) && !(value instanceof Double)) {
-            log.info("Event.put: Key={}, Value-class IS NOT DOUBLE: {}", key, value.getClass().getName());
-            Double newVal;
-            if (value instanceof Number) {
-                log.info("Event.put: Key={}, Value-class IS NUMBER: {}", key, value.getClass().getName());
-                newVal = ((Number) value).doubleValue();
-            } else {
-                log.info("Event.put: Key={}, Value-class IS NOT NUMBER: {}", key, value.getClass().getName());
-                String s = value.toString();
-                log.info("Event.put: Key={}, Value-STR: {}", key, s);
-                if (s.charAt(0)=='"' && s.charAt(s.length()-1)=='"'
-                        || s.charAt(0)=='\'' && s.charAt(s.length()-1)=='\'')
-                    s = s.substring(1, s.length()-1);
-                log.info("Event.put: Key={}, Value-STR-without-quotes: {}", key, s);
-                newVal = Double.parseDouble(s);
-            }
-            log.info("Event.put: Key={}, New-Value={}", key, newVal);
-            return super.put(key, newVal);
-        }
-        log.info("Event.put: Key={}, Value-AS-IS={}", key, value);
-        return super.put(key, value);
-    }
 
+    // Parse from string
     public static EventMap parseEventMap(String s) {
         if (s==null) return null;
         s = s.trim();
@@ -82,18 +99,68 @@ public class EventMap extends HashMap<String, Object> implements Serializable {
         String[] pairs = s.split(",");
         EventMap eventMap = new EventMap();
         for (String pair : pairs) {
+            if (StringUtils.isBlank(pair))
+                continue;
             String[] kv = pair.split("[:=]", 2);
-            eventMap.put(kv[0], kv[1]);
+            if (kv.length==2)
+                eventMap.put(kv[0], kv[1]);
+            else
+                eventMap.put(kv[0], null);
         }
         return eventMap;
     }
 
-    public static String[] getPropertyNames() {
-        return PROPERTY_NAMES_ARRAY;
+
+    // Methods overridden
+    @Override
+    public Object put(String key, Object value) {
+        log.trace("EventMap.put(): BEGIN: key={}, value={}", key, value);
+        key = removeQuotes(key);
+        log.trace("EventMap.put(): KEY with Quotes Stripped: key={}", key);
+        EventField field = STANDARD_EVENT_FIELDS_MAP.get(key);
+        if (field!=null) {
+            log.trace("EventMap.put(): STANDARD_EVENT_FIELD: key={}, value={}", key, value);
+            if (value==null) {
+                log.trace("EventMap.put(): NULL VALUE: key={}, field-is-nullable={}", key, field.isNullable());
+                if (!field.isNullable())
+                    throw new NullPointerException("Event field cannot be null: " + key);
+                log.trace("EventMap.put(): NULL VALUE: key={}, skip-if-null={}", key, field.isSkillIfNull());
+                if (field.isSkillIfNull())
+                    return null;
+                log.trace("EventMap.put(): BEFORE DEFAULT: key={}, this={}", key, this);
+                value = field.getDefaultValue().apply(this);
+                log.trace("EventMap.put(): AFTER DEFAULT: key={}, default={}", key, value);
+            }
+            Class<?> c = field.getType();
+            log.trace("EventMap.put(): VALUE TYPE: key={}, value={}, value-type={}", key, value, value.getClass());
+            if (!c.isAssignableFrom(value.getClass())) {
+                log.trace("EventMap.put(): NOT ASSIGNABLE VALUE TYPE: key={}, field-type={}, value-type={}", key, c, value.getClass());
+                if (c==String.class)
+                    value = value.toString();
+                else if (c==Integer.class)
+                    value = Integer.parseInt(removeQuotes(value));
+                else if (c==Long.class)
+                    value = Long.parseLong(removeQuotes(value));
+                else if (c==Double.class)
+                    value = Double.parseDouble(removeQuotes(value));
+                else
+                    throw new IllegalArgumentException(String.format(
+                            "Event field type differs from provided value: field-type=%s, value-type=%s, value=%s",
+                                    c.getName(), value.getClass().getName(), value));
+                log.trace("EventMap.put(): VALUE AFTER CONVERSION: key={}, value={}, value-type={}", key, value, value.getClass());
+            }
+        }
+        log.debug("EventMap.put(): PUTTING: Key={}, Value={}", key, value);
+        return super.put(key, value);
     }
 
-    public static Class[] getPropertyClasses() {
-        return PROPERTY_CLASSES_ARRAY;
+    protected static String removeQuotes(@NonNull Object o) {
+        String s = o.toString();
+        int l = s.length()-1;
+        s = (s.charAt(0)=='"' && s.charAt(l)=='"' || s.charAt(0)=='\'' && s.charAt(l)=='\'')
+                ? s.substring(1, l) : s;
+        log.trace("EventMap.removeQuotes(): INPUT={}, RESULT={}", o, s);
+        return s;
     }
 
     public String toString() {
