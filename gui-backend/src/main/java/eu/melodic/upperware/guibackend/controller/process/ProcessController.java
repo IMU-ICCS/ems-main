1a88dd9c9e768b9318a8e9d8f602f91c85a3d420
Marta Różańska <mrozanska@7bulls.com>
2021-09-10 09:56:35 +0200

    fix for ProcessController

--- gui-backend/src/main/java/eu/melodic/upperware/guibackend/controller/process/ProcessController.java
+++ gui-backend/src/main/java/eu/melodic/upperware/guibackend/controller/process/ProcessController.java
@@ -169,7 +169,7 @@ public class ProcessController {
     }
 
     @GetMapping("/deployment/job")
-    public List<org.activeeon.morphemic.model.Job> getJobsList() {
+    public List<eu.passage.upperware.commons.model.internal.Job> getJobsList() {
         log.info("GET Cloudiator jobs list");
         final List<org.activeeon.morphemic.model.Job> allJobs = proactiveClientServiceGUI.getAllJobs();
         log.info("ProcessController->getJobsList collected jobs: {}", allJobs);
