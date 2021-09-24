ba6b3b74f3b5a7857718fc7c5b75b17d6dcf678b
ipatini <ipatini@mail.ntua.gr>
2021-09-24 12:37:05 +0300

    EMS: Baguette Client: Added joinedCluster and leftCluster methods in NodeCallback interface and classes implementing it.

--- event-management/baguette-client/src/main/java/eu/melodic/event/baguette/client/cluster/ClusterManager.java
+++ event-management/baguette-client/src/main/java/eu/melodic/event/baguette/client/cluster/ClusterManager.java
@@ -204,6 +204,8 @@ public class ClusterManager extends AbstractLogBase {
 			if (isInitialized() && isRunning()) break;
 			try { Thread.sleep(500); } catch (InterruptedException e) { break; }
 		}
+		if (callback!=null)
+			callback.joinedCluster();
 	}
 
 	public void waitToJoin(long waitForMillis) {
@@ -214,6 +216,8 @@ public class ClusterManager extends AbstractLogBase {
 			long waitFor = Math.min(500, endTm-System.currentTimeMillis());
 			try { Thread.sleep(waitFor); } catch (InterruptedException e) { break; }
 		}
+		if (callback!=null)
+			callback.joinedCluster();
 	}
 
 	public void leaveCluster() {
@@ -226,6 +230,8 @@ public class ClusterManager extends AbstractLogBase {
 		log_debug("CLM: Left cluster in {}ms", endTm-startTm);
 		atomix = null;
 		brokerUtil = null;
+		if (callback!=null)
+			callback.leftCluster();
 	}
 
 	// ------------------------------------------------------------------------
