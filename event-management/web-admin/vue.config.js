be9d0f90da60ce672e64835cadf43457f225c0e1
ipatini <ipatini@mail.ntua.gr>
2022-01-03 10:41:24 +0200

    EMS: Web Admin: Reverted change in vue.config.js 'proxy.target' property

--- event-management/web-admin/vue.config.js
+++ event-management/web-admin/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^': {
-        target: process.env.NODE_ENV === 'production' ? 'https://localhost:8111' : 'http://localhost:8111',
+        target: 'https://localhost:8111',
         ws: true,
         changeOrigin: true
       },
