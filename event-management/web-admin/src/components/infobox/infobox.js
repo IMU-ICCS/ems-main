362cb4123116ab208044ba8774b7df04ba83d611
ipatini <ipatini@acci.gr>
2022-01-20 14:45:03 +0200

    EMS: Web Admin: Made InfoBox component more configurable

--- event-management/web-admin/src/components/infobox/infobox.js
+++ event-management/web-admin/src/components/infobox/infobox.js
@@ -4,12 +4,14 @@ export default {
   props: {
     value: String,
     id: String,
+    classes: String,
     message: String,
     text_before: String,
     text_default: String,
     text_after: String,
     bg_class: String,
     icon_classes: String,
-    loading: Boolean
+    loading: Boolean,
+    style: { type: String, default: 'padding:0;' }
   }
 }
