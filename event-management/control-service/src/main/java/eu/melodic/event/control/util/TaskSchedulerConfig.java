4adfee7d412526cd5f58ca7afc1fe24dd69b5b3d
ipatini <ipatini@acci.gr>
2022-02-07 21:57:11 +0200

    EMS: Control Service: Added TaskSchedulerConfig class to configure task scheduler thread pool size

---
+++ event-management/control-service/src/main/java/eu/melodic/event/control/util/TaskSchedulerConfig.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017-2022 Institute of Communication and Computer Systems (imu.iccs.gr)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v2.0, unless
+ * Esper library is used, in which case it is subject to the terms of General Public License v2.0.
+ * If a copy of the MPL was not distributed with this file, you can obtain one at
+ * https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+package eu.melodic.event.control.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import javax.validation.constraints.Min;
+
+@Slf4j
+@Configuration
+@EnableScheduling
+public class TaskSchedulerConfig {
+    @Value("${control.task-scheduler.thread-pool-size:16}")
+    @Min(1)
+    private int threadPoolSize;
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        log.info("TaskSchedulerConfig: TaskScheduler thread pool size: {}", threadPoolSize);
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(threadPoolSize);
+        return threadPoolTaskScheduler;
+    }
+}
