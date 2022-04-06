53611522fd40e6534b3f017bbb306fc82c7ae7d7
ipatini <ipatini@acci.gr>
2022-04-06 16:35:54 +0300

    EMS: Control Service: Minor change

--- event-management/control-service/src/main/java/eu/melodic/event/control/ControlServiceApplication.java
+++ event-management/control-service/src/main/java/eu/melodic/event/control/ControlServiceApplication.java
@@ -32,14 +32,14 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import java.util.Timer;
 import java.util.TimerTask;
 
+@Slf4j
+@EnableAsync
+@Configuration
 @SpringBootApplication(
         scanBasePackages = {"eu.melodic.event.baguette.server", "eu.melodic.event.baguette.client.install",
                 "eu.melodic.event.baguette.client.selfhealing", "eu.melodic.event.brokercep", "eu.melodic.event.control",
                 "eu.melodic.event.translate", "eu.melodic.event.common", "eu.melodic.event.util"},
         exclude = { SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class } )
-@EnableAsync
-@Configuration
-@Slf4j
 public class ControlServiceApplication /*implements ApplicationContextAware*/ {
     private static ConfigurableApplicationContext applicationContext;
     private static Timer exitTimer;
