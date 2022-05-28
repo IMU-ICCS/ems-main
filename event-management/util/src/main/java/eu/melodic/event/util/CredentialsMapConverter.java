9b10a368399d26e3aaf8aca65b4436bd11dfe47a
ipatini <ipatini@acci.gr>
2022-05-28 18:54:44 +0300

    EMS: Util: Added class CredentialsMapConverter for converting String property values to CredentialsMap objects (used in Baguette Server)

---
+++ event-management/util/src/main/java/eu/melodic/event/util/CredentialsMapConverter.java
@@ -0,0 +1,55 @@
+
+/*
+ * Copyright (C) 2017-2022 Institute of Communication and Computer Systems (imu.iccs.gr)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v2.0, unless
+ * Esper library is used, in which case it is subject to the terms of General Public License v2.0.
+ * If a copy of the MPL was not distributed with this file, you can obtain one at
+ * https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+package eu.melodic.event.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ *  Converts a String to a CredentialsMap
+ */
+@Slf4j
+@Component
+@ConfigurationPropertiesBinding
+public class CredentialsMapConverter implements Converter<String, CredentialsMap> {
+    private Gson gson;
+
+    @PostConstruct
+    public void initObjectMapper() {
+        gson = new Gson();
+    }
+
+    @Override
+    public CredentialsMap convert(@NonNull String s) {
+        if (StringUtils.isNotBlank(s)) {
+            try {
+                CredentialsMap credentialsMap = gson.fromJson(s.trim(), CredentialsMap.class);
+                log.debug("CredentialsMapConverter: result: {}", credentialsMap);
+                return credentialsMap;
+            } catch (Throwable t) {
+                log.debug("CredentialsMapConverter: JSON input: {}", s);
+                log.error("CredentialsMapConverter: EXCEPTION while parsing JSON input: ", t);
+                throw new IllegalArgumentException(t);
+            }
+        }
+        throw new IllegalArgumentException("Input is blank: "+s);
+    }
+}
