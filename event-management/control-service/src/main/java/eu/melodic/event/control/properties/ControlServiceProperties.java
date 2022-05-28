951b3a69652926541cf5292390e4730efb982bc9
ipatini <ipatini@acci.gr>
2022-05-28 13:42:02 +0300

    EMS: Control Service: Removed PropertySource annotation from ControlServiceProperties therefore run.* scripts must provide the (list of) configuration files, or name config. file application.yml

--- event-management/control-service/src/main/java/eu/melodic/event/control/properties/ControlServiceProperties.java
+++ event-management/control-service/src/main/java/eu/melodic/event/control/properties/ControlServiceProperties.java
@@ -15,7 +15,7 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+//import org.springframework.context.annotation.PropertySource;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.Min;
@@ -29,12 +29,12 @@ import static eu.melodic.event.util.EmsConstant.EMS_PROPERTIES_PREFIX;
 @Validated
 @Configuration
 @ConfigurationProperties(prefix = EMS_PROPERTIES_PREFIX + "control")
-@PropertySource(value = {
+/*@PropertySource(value = {
         "file:${MELODIC_CONFIG_DIR}/ems-server.yml",
         "file:${MELODIC_CONFIG_DIR}/ems-server.properties",
         "file:${MELODIC_CONFIG_DIR}/ems.yml",
         "file:${MELODIC_CONFIG_DIR}/ems.properties"
-}, ignoreResourceNotFound = true)
+}, ignoreResourceNotFound = true)*/
 public class ControlServiceProperties {
     public enum IpSetting {
         DEFAULT_IP,
