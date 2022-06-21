293a2940b7984ff267ff43ba95c8c4ca5990f6d1
ipatini <ipatini@acci.gr>
2022-06-21 21:31:32 +0300

    MetaSolver: Added 'cputil.bat' script for easier CP model loading to CDO repository

---
+++ meta_solver/cputil.bat
@@ -0,0 +1,17 @@
+::
+:: Copyright (C) 2017-2022 Institute of Communication and Computer Systems (imu.iccs.gr)
+::
+:: This Source Code Form is subject to the terms of the Mozilla Public License, v2.0.
+:: If a copy of the MPL was not distributed with this file, You can obtain one at
+:: https://www.mozilla.org/en-US/MPL/2.0/
+::
+
+:: Usage:
+:: cputil.bat "file:/path/to/cp-model.xmi" "/cp-model"
+
+@echo off
+set curdir=%~dp0
+IF NOT DEFINED MELODIC_CONFIG_DIR set MELODIC_CONFIG_DIR=%curdir%\src\main\resources\config
+IF NOT DEFINED PAASAGE_CONFIG_DIR set PAASAGE_CONFIG_DIR=%curdir%\src\main\resources
+
+java -cp target\meta-solver.jar -Dloader.main=eu.melodic.upperware.metasolver.util.UtilCpModelImport org.springframework.boot.loader.PropertiesLauncher 1 "file:%1" "%2"
