718b20269533a2553d22f14a6010959201545589
mriedl <maciej.riedl@gmail.com>
2022-10-04 11:20:10 +0200

    Added description to the exception

--- melodic-cache-standalone/src/main/java/eu/melodic/upperware/cachestandalone/exception/NodeCandidatesNotFound.java
+++ melodic-cache-standalone/src/main/java/eu/melodic/upperware/cachestandalone/exception/NodeCandidatesNotFound.java
@@ -6,4 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NodeCandidatesNotFound extends RuntimeException {
 
+    public NodeCandidatesNotFound() {
+        super("Node Candidates not available");
+    }
 }
