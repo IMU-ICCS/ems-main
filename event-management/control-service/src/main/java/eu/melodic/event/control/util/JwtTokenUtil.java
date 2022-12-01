c5d4536097e80a010a87660c5681e6d6402b9df6
ipatini <ipatini@mail.ntua.gr>
2022-12-01 12:11:55 +0200

    EMS: Control Service: Fixed JwtTokenUtil in order to allow both JwtTokenUtil and ControlServiceApplication boot (see bin/jwtutil.* and bin/run.* scripts respectively)

--- event-management/control-service/src/main/java/eu/melodic/event/control/util/JwtTokenUtil.java
+++ event-management/control-service/src/main/java/eu/melodic/event/control/util/JwtTokenUtil.java
@@ -17,6 +17,7 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -26,6 +27,7 @@ import org.springframework.context.annotation.ComponentScan;
  * java -cp .\target\control-service.jar  -Dloader.main=eu.melodic.event.control.util.JwtTokenUtil -Dlogging.level.ROOT=WARN -Dlogging.level.eu.melodic.event.control.util.JwtTokenUtil=INFO org.springframework.boot.loader.PropertiesLauncher parser [TOKEN]
  */
 @Slf4j
+@SpringBootApplication
 @ComponentScan(basePackages = { "eu.paasage.upperware.security.authapi" })
 @RequiredArgsConstructor
 public class JwtTokenUtil implements CommandLineRunner {
@@ -40,7 +42,7 @@ public class JwtTokenUtil implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         if (args.length>0) {
             String token;
             if ("create".equalsIgnoreCase(args[0].trim())) {
