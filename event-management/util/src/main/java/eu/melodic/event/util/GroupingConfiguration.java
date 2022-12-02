be1c203f40992976524c0478260ca99956fe92de
ipatini <ipatini@mail.ntua.gr>
2022-12-02 11:43:10 +0200

    EMS: Util, Common: Changes to reduce clutter in logs (i.e. Removed certificates logging in GroupingConfiguration, changed log level to 'debug' in periodically repeating messages in AbstractEndpointCollector)

--- event-management/util/src/main/java/eu/melodic/event/util/GroupingConfiguration.java
+++ event-management/util/src/main/java/eu/melodic/event/util/GroupingConfiguration.java
@@ -26,20 +26,20 @@ import java.util.Set;
 @ToString(exclude = {"brokerPassword"})
 public class GroupingConfiguration implements Serializable {
     @NonNull private String name;
-    /*@NonNull*/ private Properties properties;
+    private Properties properties;
     @NonNull private Map<String, BrokerConnectionConfig> brokerConnections;
     @NonNull private Set<String> eventTypeNames;
     @NonNull private Map<String, Set<String>> rules;
     @NonNull private Map<String, Set<String>> connections;
     @NonNull private Set<FunctionDefinition> functionDefinitions;
     @NonNull private Map<String, Double> constants;
-    /*@NonNull*/ private String brokerUsername;
-    /*@NonNull*/ private String brokerPassword;
+    private String brokerUsername;
+    private String brokerPassword;
 
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
-    @ToString(exclude = {/*"certificate",*/ "password"})
+    @ToString(exclude = {"certificate", "password"})
     public static class BrokerConnectionConfig implements Serializable {
         private String grouping;
         private String url;
