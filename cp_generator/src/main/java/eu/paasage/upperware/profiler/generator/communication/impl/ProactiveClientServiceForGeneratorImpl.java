6bd05eb11f09c63f61e79989a926e7b17db97525
mriedl <mriedl@7bulls.com>
2023-02-10 14:32:52 +0100

    Added logging when retrying for chcking if nodeCandidates are downlaoded

--- cp_generator/src/main/java/eu/paasage/upperware/profiler/generator/communication/impl/ProactiveClientServiceForGeneratorImpl.java
+++ cp_generator/src/main/java/eu/paasage/upperware/profiler/generator/communication/impl/ProactiveClientServiceForGeneratorImpl.java
@@ -9,22 +9,38 @@ import org.ow2.proactive.sal.model.NodeCandidate;
 import org.ow2.proactive.sal.model.Requirement;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 public class ProactiveClientServiceForGeneratorImpl implements ProactiveClientServiceForGenerator {
 
+    private static final int NUMBER_OF_REPEATS_FOR_NODE_CANDIDATES = 20;
+    private static final int DELAY_BETWEEN_REQUESTS = 5000;
     private final ProactiveClientConnectorService proactiveClientConnectorService;
 
     @Override
     public List<NodeCandidate> findNodeCandidates(List<Requirement> requirements) {
-
+        List<NodeCandidate> nodeCandidates = new LinkedList<>();
+        boolean isAnyAsyncNodeCandidatesProcessesInProgress = true;
+        int requestNo = 0;
         try {
-            return proactiveClientConnectorService.fetchNodeCandidates(requirements);
-        } catch (ProactiveClientException e) {
-            log.error("Error message body: {}", e.getMessage());
-            return Collections.emptyList();
+            while (isAnyAsyncNodeCandidatesProcessesInProgress && (requestNo < NUMBER_OF_REPEATS_FOR_NODE_CANDIDATES)) {
+                log.info("Checking if nodeCandidates downlaod process is finished. Trye: {}", requestNo);
+                isAnyAsyncNodeCandidatesProcessesInProgress = proactiveClientConnectorService.isAnyAsyncNodeCandidatesProcessesInProgress();
+                Thread.sleep(DELAY_BETWEEN_REQUESTS);
+                requestNo++;
+            }
+            if (isAnyAsyncNodeCandidatesProcessesInProgress) {
+                throw new RuntimeException("NodeCandidates are not yet present inside proactive scheduler");
+            }
+            nodeCandidates = proactiveClientConnectorService.fetchNodeCandidates(requirements);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        } catch (ProactiveClientException e2) {
+            log.error("Error message body: {}", e2.getMessage());
         }
+        return nodeCandidates;
     }
 }
--- gui-backend/src/main/java/eu/melodic/upperware/guibackend/communication/proactive/ProactiveClientServiceImpl.java
+++ gui-backend/src/main/java/eu/melodic/upperware/guibackend/communication/proactive/ProactiveClientServiceImpl.java
@@ -3,10 +3,12 @@ package eu.melodic.upperware.guibackend.communication.proactive;
 import cloud.morphemic.connectors.ProactiveClientConnectorService;
 import cloud.morphemic.connectors.exception.ProactiveClientException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Collections;
 
 @RequiredArgsConstructor
+@Slf4j
 public class ProactiveClientServiceImpl implements ProactiveClientService {
 
     private final ProactiveClientConnectorService proactiveClientConnectorService;
