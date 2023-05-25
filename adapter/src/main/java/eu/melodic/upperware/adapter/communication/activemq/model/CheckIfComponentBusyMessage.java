707bb0611674283772a3eec604f1095bc2b886bc
mriedl <mriedl@7bulls.com>
2023-05-25 15:34:09 +0200

    Changed busy metric to handle netricValue field and cast numeric values to enum

--- adapter/src/main/java/eu/melodic/upperware/adapter/communication/activemq/model/CheckIfComponentBusyMessage.java
+++ adapter/src/main/java/eu/melodic/upperware/adapter/communication/activemq/model/CheckIfComponentBusyMessage.java
@@ -1,9 +1,9 @@
 package eu.melodic.upperware.adapter.communication.activemq.model;
 
 import com.google.gson.annotations.SerializedName;
-import com.sun.istack.NotNull;
 import eu.melodic.upperware.adapter.service.Instance_no_provider.InstanceStatus;
 import lombok.Data;
+
 
 @Data
 public class CheckIfComponentBusyMessage {
@@ -11,7 +11,6 @@ public class CheckIfComponentBusyMessage {
     @SerializedName("instanceName")
     private String componentInstanceName;
 
-    @SerializedName("instanceStatus")
-    @NotNull
+    @SerializedName("metricValue")
     private InstanceStatus instanceStatus;
 }
