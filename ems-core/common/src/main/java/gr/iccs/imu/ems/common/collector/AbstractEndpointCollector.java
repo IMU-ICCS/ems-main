ffedc954a74286af814c8d19a903e7f9dd01daa8
ipatini <ipatini@mail.ntua.gr>
2025-01-31 02:34:52 +0200

    EMS: Reverted changes in AbstractEndpointCollector

--- ems-core/common/src/main/java/gr/iccs/imu/ems/common/collector/AbstractEndpointCollector.java
+++ ems-core/common/src/main/java/gr/iccs/imu/ems/common/collector/AbstractEndpointCollector.java
@@ -80,11 +80,11 @@ public abstract class AbstractEndpointCollector<T> implements InitializingBean, 
 
     public void processAllowedTopics(Collection<String> allowedTopicsSpec) {
         Set<String> topicsSet = allowedTopicsSpec == null
-                ? null : allowedTopicsSpec.stream()
+                ? null : properties.getAllowedTopics().stream()
                         .map(s -> s.split(":")[0])
                         .collect(Collectors.toSet());
-        Map<String, Set<String>> topicsMap = allowedTopicsSpec == null
-                ? null : allowedTopicsSpec.stream()
+        Map<String, Set<String>> topicsMap = properties.getAllowedTopics() == null
+                ? null : properties.getAllowedTopics().stream()
                         .map(s -> s.split(":", 2))
                         .collect(Collectors.groupingBy(a -> a[0],
                                 Collectors.mapping(a -> a.length > 1 ? a[1] : "", Collectors.toSet())));
