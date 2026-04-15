54a7792390b872fc109b3afa90dd52051b6bf58e
ipatini <ipatini@mail.ntua.gr>
2026-04-15 09:04:30 +0300

    Updated copyright notice (2017-2026)

--- ems-core/util/src/main/java/gr/iccs/imu/ems/util/EmsRelease.java
+++ ems-core/util/src/main/java/gr/iccs/imu/ems/util/EmsRelease.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2023 Institute of Communication and Computer Systems (imu.iccs.gr)
+ * Copyright (C) 2017-2026 Institute of Communication and Computer Systems (imu.iccs.gr)
  *
  * This Source Code Form is subject to the terms of the Mozilla Public License, v2.0, unless
  * Esper library is used, in which case it is subject to the terms of General Public License v2.0.
@@ -17,7 +17,7 @@ public class EmsRelease {
     public final static String EMS_NAME = "Event Management System";
     public final static String EMS_VERSION = EmsRelease.class.getPackage().getImplementationVersion();
     public final static String EMS_COPYRIGHT =
-            "Copyright (C) 2017-2025 Institute of Communication and Computer Systems (imu.iccs.gr)";
+            "Copyright (C) 2017-2026 Institute of Communication and Computer Systems (imu.iccs.gr)";
     public final static String EMS_LICENSE = "Mozilla Public License, v2.0";
     public final static String EMS_DESCRIPTION = String.format("\n%s (%s), v.%s, %s\n%s\n",
             EMS_NAME, EMS_ID, EMS_VERSION, EMS_LICENSE, EMS_COPYRIGHT);
